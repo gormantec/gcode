e28cd7c7b339789047f3d63202a5889e93ba101e
craig <craig@craigs-iMac.local>
2020-07-04 14:17:31 +1000

    u

--- js/code.js
+++ js/code.js
@@ -23,7 +23,7 @@ class PWA {
         var g = (rgb >> 8) & 0xff;  // extract green
         var b = (rgb >> 0) & 0xff;  // extract blue
         var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
-        if (luma < 50) {
+        if (luma < 40) {
             // pick a different colour
             return "#F0F0F0";
         }
