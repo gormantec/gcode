f700d9177412579b502122b8b58d7fdef1f7237f
craig <craig@craigs-iMac.local>
2020-07-04 14:14:50 +1000

    u

--- js/code.js
+++ js/code.js
@@ -17,7 +17,7 @@ class PWA {
 
     getTextColor(backColor) {
         
-        var backColor = c.substring(1);      // strip #
+        var backColor = backColor.substring(1);      // strip #
         var rgb = parseInt(backColor, 16);   // convert rrggbb to decimal
         var r = (rgb >> 16) & 0xff;  // extract red
         var g = (rgb >> 8) & 0xff;  // extract green
