b3284e6795b7657da2aa625e7656326f5d8fe2f6
craig <craig@craigs-iMac.local>
2020-07-25 19:42:55 +1000

    xxx

--- modules/pwa.js
+++ modules/pwa.js
@@ -177,6 +177,7 @@ class Div {
         if (params && params.backgroundPosition) this.element.style.backgroundPosition = params.backgroundPosition;
         if (params && params.backgroundRepeat) this.element.style.backgroundRepeat = params.backgroundRepeat;
         if (params && params.backgroundImage) this.element.style.backgroundImage = params.backgroundImage;
+        this.element.className=(this.element.className+" pwaPage").trim();
         //if (params.style) this.element.setAttribute("style",params.style);
     }
     onclick(afunc) {
