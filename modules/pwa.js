2c893e6184a15e57263e5884530457358ce5767d
craig <craig@craigs-iMac.local>
2020-07-28 16:11:32 +1000

    xxx

--- modules/pwa.js
+++ modules/pwa.js
@@ -213,9 +213,9 @@ class Div {
             this.element.innerText = params.innerText;
         }
         else if (params && params.innerHTML  && params.innerHTML.substring(0,4).toLowerCase()=="url("){
-            var _uri=params.innerHTML.substring(4,params.innerHTML.length-1);
-            _uri=_uri.sub(/\A"(.*)"\z/, '\\1')
-            _uri=_uri.sub(/\A'(.*)'\z/, '\\1')
+            var _uri=params.innerHTML.trim().substring(4,params.innerHTML.length-1);
+            _uri=_uri.sub(/\A"(.*)"\z/, '\\1');
+            _uri=_uri.sub(/\A'(.*)'\z/, '\\1');
             console.log(_uri);
             var _this=this;
             fetch(params.innerHTML.substring(4,params.innerHTML.length-1))
