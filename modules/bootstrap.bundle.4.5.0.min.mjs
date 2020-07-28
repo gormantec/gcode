37ca140fbadd9db5f3ff998106c8cea4fb347022
craig <craig@craigs-iMac.local>
2020-07-28 17:11:59 +1000

    xxx

--- modules/bootstrap.bundle.4.5.0.min.mjs
+++ modules/bootstrap.bundle.4.5.0.min.mjs
@@ -1,6 +1,11 @@
 import "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js";
 import "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js";
 
+var _style = window.document.createElement("link");
+_style.setAttribute("rel", "stylesheet");
+_style.setAttribute("href", "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
+window.document.head.appendChild(_style);
+
 window.Util = Util;
 window.Alert = Alert;
 window.Button = Button;
