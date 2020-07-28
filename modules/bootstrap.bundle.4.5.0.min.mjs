6ad9960cf0610af69c8ad7ae44a0d28c4252b19f
craig <craig@craigs-iMac.local>
2020-07-28 17:17:51 +1000

    dddd

--- modules/bootstrap.bundle.4.5.0.min.mjs
+++ modules/bootstrap.bundle.4.5.0.min.mjs
@@ -1,5 +1,8 @@
-import "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js";
-import "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js";
+import { Popover } from "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js";
+
+window.Popover = Popover;
+
+import {Util, Alert, Button, Carousel, Collapse, Dropdown, Modal, ScrollSpy, Tab, Tooltip} from "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js";
 
 var _style = window.document.createElement("link");
 _style.setAttribute("rel", "stylesheet");
@@ -13,7 +16,6 @@ window.Carousel = Carousel;
 window.Collapse = Collapse;
 window.Dropdown = Dropdown;
 window.Modal = Modal;
-window.Popover = Popover;
 window.Scrollspy = ScrollSpy;
 window.Tab = Tab;
 window.Tooltip = Tooltip;
