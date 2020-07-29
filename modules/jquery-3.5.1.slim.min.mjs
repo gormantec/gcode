c6c876eef4cbd50734b4055ef2a641bbaf84d881
craig <craig@craigs-iMac.local>
2020-07-29 14:44:50 +1000

    xxx

--- modules/jquery-3.5.1.slim.min.mjs
+++ modules/jquery-3.5.1.slim.min.mjs
@@ -1,5 +1,8 @@
 import 'https://code.jquery.com/jquery-3.5.1.slim.min.js';
 console.log($);
+console.log("exports:"+exports);
+console.log("module:"+module);
+console.log("module.exports:"+module.exports);
 var $ = $ || {};
 console.log($);
 window.$ = $;
