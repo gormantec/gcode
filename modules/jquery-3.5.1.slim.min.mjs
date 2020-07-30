fdb2d7c968d62b107f1ab0ba78183f8534b048af
craig <craig@craigs-iMac.local>
2020-07-30 12:40:31 +1000

    xxx

--- modules/jquery-3.5.1.slim.min.mjs
+++ modules/jquery-3.5.1.slim.min.mjs
@@ -1,19 +1,3 @@
 console.log("modules/jquery-3.5.1.slim.min.mjs");
-
-var exports,module;
-
 import 'https://code.jquery.com/jquery-3.5.1.slim.min.js';
 export default window.jQuery.noConflict(true);
-console.log($);
-console.log("exports:"+exports);
-console.log("module:"+module);
-if(module) console.log("module.exports:"+module.exports);
-console.log("this.JQuery:"+this.JQuery);
-console.log("window:"+window);
-if(window) console.log("window.JQuery:"+window.JQuery);
-if(window) console.log("window.module:"+window.module);
-if(window && window.module) console.log("window.module.exports:"+window.module.exports);
-var $ = $ || {};
-console.log($);
-window.$ = $;
-export { $ };
