cde98de474b3ce474df8a1a2c0ef47016418ba33
craig <craig@craigs-iMac.local>
2020-07-30 22:23:25 +1000

    xxxx

--- modules/bootstrap.bundle.4.5.0.min.mjs
+++ modules/bootstrap.bundle.4.5.0.min.mjs
@@ -2,8 +2,8 @@ console.log("modules/bootstrap.bundle.4.5.0.min.mjs");
 import 'https://code.jquery.com/jquery-3.5.1.js';
 var jQuery = window.jQuery;
 var $ = jQuery;
-import "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js";
-import "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js";
+import "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.js";
+import "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.js";
 
 var Util = window.Util;
 var Alert = window.Alert;
