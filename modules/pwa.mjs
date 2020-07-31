3172af7f2d65c556784d07db988d100cc6e236eb
craig <craig@craigs-iMac.local>
2020-08-01 09:10:11 +1000

    xxx

--- modules/pwa.mjs
+++ modules/pwa.mjs
@@ -271,6 +271,9 @@ class Div {
         if (params && params.bottom) this.element.style.bottom = params.bottom;
         if (params && params.left) this.element.style.left = params.left;
         if (params && params.right) this.element.style.right = params.right;
+        if (params && params.borderRadius) this.element.style.borderRadius = params.borderRadius;
+        if (params && params.fontSize) this.element.style.fontSize = params.fontSize;
+        if (params && params.padding) this.element.style.right = params.padding;
         if (params && params.width) {
             this.element.style.width = params.width;
             if(!params.right)this.element.style.right=unset;
@@ -285,6 +288,9 @@ class Div {
         if (params && params.backgroundPosition) this.element.style.backgroundPosition = params.backgroundPosition;
         if (params && params.backgroundRepeat) this.element.style.backgroundRepeat = params.backgroundRepeat;
         if (params && params.backgroundImage) this.element.style.backgroundImage = params.backgroundImage;
+        
+        
+        padding
         if(!params || !params.tagName)this.element.className = (this.element.className + " pwadiv").trim();
         //if (params.style) this.element.setAttribute("style",params.style);
     }
