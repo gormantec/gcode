7be6c7d56fbaa57a2f7b1383ffd286466431546a
craig <craig@craigs-iMac.local>
2020-08-01 01:36:43 +1000

    xxx

--- modules/pwa.mjs
+++ modules/pwa.mjs
@@ -273,13 +273,13 @@ class Div {
         if (params && params.right) this.element.style.right = params.right;
         if (params && params.width) {
             this.element.style.width = params.width;
-            if(!params.right)this.element.style.right="";
-            if(!params.left)this.element.style.left="";
+            if(!params.right)this.element.style.right=null;
+            if(!params.left)this.element.style.left=null;
         }
         if (params && params.height){
             this.element.style.height = params.height;
-            if(!params.top)this.element.style.top="";
-            if(!params.bottom)this.element.style.bottom="";
+            if(!params.top)this.element.style.top=null;
+            if(!params.bottom)this.element.style.bottom=null;
         }
         if (params && params.backgroundColor) this.element.style.backgroundColor = params.backgroundColor;
         if (params && params.backgroundPosition) this.element.style.backgroundPosition = params.backgroundPosition;
