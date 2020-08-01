bd99c25b46f354e0f15df8e8fc04fbd408a42873
craig <craig@craigs-iMac.local>
2020-08-01 12:41:10 +1000

    xxxx

--- modules/main.mjs
+++ modules/main.mjs
@@ -326,7 +326,7 @@ function _toggleSideBar() {
         document.getElementById("pageMiddle").style.left = (leftToolbarWidth + leftPageWidth + 2) + "px";
         document.getElementById("filename").style.marginLeft = (leftToolbarWidth + leftPageWidth + 22) + "px";
         document.getElementById("runHeaderButton").style.left = (leftToolbarWidth + leftPageWidth + 2) + "px";
-        document.getElementById("sideBarButton").getElementsByTagName("i")[0].innerText = "keyboard_arrow_down";
+        document.getElementById("sideBarButton").getElementsByTagName("i")[0].innerText = "keyboard_arrow_left";
     }
 
 }
