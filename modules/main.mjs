e8d19f56e7c34d830cabb92a0ca81a3b9e10933b
craig <craig@craigs-iMac.local>
2020-08-07 11:00:53 +1000

    xxxx

--- modules/main.mjs
+++ modules/main.mjs
@@ -450,45 +450,50 @@ function _toolbarButtonClicked() {
             if(!e)
             {
                 fetch("https://5q7l0c3xq9.execute-api.ap-southeast-2.amazonaws.com?code="+code+"&state="+guid).then(
-                    response => response.text()
-                ).then((text) => { console.log(text);});
-        
-                var gitRepoName = prompt("Git username/repo");
-                var username = gitRepoName.substring(0, gitRepoName.indexOf("/"));
-                var repo = gitRepoName.substring(gitRepoName.indexOf("/") + 1);
-        
-                var gitRepositories = localStorage.getItem("git-repositories");
-                var data = {};
-                if (!gitRepositories) {
-                    data = {};
-                    data["git://" + username + ":" + repo] = { "username": username, "repo": repo };
-                    localStorage.setItem("git-repositories", JSON.stringify(data));
-                }
-                else {
-                    data = JSON.parse(gitRepositories);
-                    data["git://" + username + ":" + repo] = ({ "username": username, "repo": repo });
-                    localStorage.setItem("git-repositories", JSON.stringify(data));
-                }
-                var toDiv = document.getElementById("pageLeftBody");
-                Object.values(data).forEach(function (r, x) {
-                    var running_count = 0;
-                    githubtree.pullGitRepository(r.username, r.repo, function (state, repo) {
-                        if (state == "running") {
-                            running_count++;
-                            if (Math.floor(running_count / 10) * 10 == running_count) {
+                    response => response.json()
+                ).then((json) => { 
+                    console.log("********************");
+                    console.log(json);
+
+                    var gitRepoName = prompt("Git username/repo");
+                    var username = gitRepoName.substring(0, gitRepoName.indexOf("/"));
+                    var repo = gitRepoName.substring(gitRepoName.indexOf("/") + 1);
+            
+                    var gitRepositories = localStorage.getItem("git-repositories");
+                    var data = {};
+                    if (!gitRepositories) {
+                        data = {};
+                        data["git://" + username + ":" + repo] = { "username": username, "repo": repo };
+                        localStorage.setItem("git-repositories", JSON.stringify(data));
+                    }
+                    else {
+                        data = JSON.parse(gitRepositories);
+                        data["git://" + username + ":" + repo] = ({ "username": username, "repo": repo });
+                        localStorage.setItem("git-repositories", JSON.stringify(data));
+                    }
+                    var toDiv = document.getElementById("pageLeftBody");
+                    Object.values(data).forEach(function (r, x) {
+                        var running_count = 0;
+                        githubtree.pullGitRepository(r.username, r.repo, function (state, repo) {
+                            if (state == "running") {
+                                running_count++;
+                                if (Math.floor(running_count / 10) * 10 == running_count) {
+                                    githubtree.refreshGitTree(username, repo, toDiv, filename);
+            
+                                    Array.from(toDiv.querySelector("div.dirWidget[data-name='git://" + username + ":" + repo + "']").parentElement.getElementsByClassName("dirWidget")).forEach(function (e) { e.onclick = _openDir; });
+                                    Array.from(toDiv.querySelector("div.dirWidget[data-name='git://" + username + ":" + repo + "']").parentElement.getElementsByClassName("fileWidget")).forEach(function (e) { e.onclick = _openFile; });
+                                }
+                            }
+                            if (state == "done") {
                                 githubtree.refreshGitTree(username, repo, toDiv, filename);
-        
                                 Array.from(toDiv.querySelector("div.dirWidget[data-name='git://" + username + ":" + repo + "']").parentElement.getElementsByClassName("dirWidget")).forEach(function (e) { e.onclick = _openDir; });
                                 Array.from(toDiv.querySelector("div.dirWidget[data-name='git://" + username + ":" + repo + "']").parentElement.getElementsByClassName("fileWidget")).forEach(function (e) { e.onclick = _openFile; });
                             }
-                        }
-                        if (state == "done") {
-                            githubtree.refreshGitTree(username, repo, toDiv, filename);
-                            Array.from(toDiv.querySelector("div.dirWidget[data-name='git://" + username + ":" + repo + "']").parentElement.getElementsByClassName("dirWidget")).forEach(function (e) { e.onclick = _openDir; });
-                            Array.from(toDiv.querySelector("div.dirWidget[data-name='git://" + username + ":" + repo + "']").parentElement.getElementsByClassName("fileWidget")).forEach(function (e) { e.onclick = _openFile; });
-                        }
+                        });
                     });
                 });
+        
+
             }
         });
 
