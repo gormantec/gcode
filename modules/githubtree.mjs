57dad5132ae95acb4e36e67363352a6023d61f01
craig <craig@craigs-iMac.local>
2020-08-09 14:34:13 +1000

    xxxx

--- modules/githubtree.mjs
+++ modules/githubtree.mjs
@@ -277,7 +277,7 @@ export function pullGitRepository(params, callbackrefresh) {
                 var directories = [];
                 Array.from(sha.data).forEach(function (file) {
                     if (file.name.substring(0, 1) != ".") {
-                        console.log("addRepoFile:"+file.path);
+                        console.log("addRepoFile:"+file.path +" "+file.name);
                         addRepoFile(repo, _path, { name: file.name, filepath: file.path, dirpath: _path, sha: file.sha, type: file.type });
                         if (callbackrefresh) callbackrefresh("running", repo, _path);
     
