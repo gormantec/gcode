605641d94bde32d24e2fc4ddf717e4aebc38742a
craig <craig@craigs-iMac.local>
2020-08-13 11:55:30 +1000

    xxx

--- modules/main.mjs
+++ modules/main.mjs
@@ -125,9 +125,9 @@ function _new() {
                         "filename:" + aFilename + "\n  " +
                         "created: " + (new Date(Date.now())).getFullYear() + "-" + (new Date(Date.now())).getMonth() + "-" + (new Date(Date.now())).getDay() + "T" + (new Date()).toLocaleTimeString() + "\n  " +
                         "appName: gcodeApp" + "\n  " +
-                        "splash: https://git.gormantec.com/gcode/images/android/android-launchericon-144-144.png" + "\n  " +
-                        "icon: https://git.gormantec.com/gcode/images/android/android-launchericon-180-180op.png" + "\n  " +
-                        "icon180x180: https://git.gormantec.com/gcode/images/android/android-launchericon-180-180op.png" + "\n  " +
+                        "splash: https://git.gormantec.com/gcode/images/ios/ios-appicon-180-180.png" + "\n  " +
+                        "icon: https://git.gormantec.com/gcode/images/ios/ios-appicon-180-180op.png" + "\n  " +
+                        "icon180x180: https://git.gormantec.com/gcode/images/ios/ios-appicon-180-180op.png" + "\n  " +
                         "splashBackgroundColor: #005040" + "\n  " +
                         "splashDuration: 2000" +
                         "\n\n*/\n\n" + _samplecode);
