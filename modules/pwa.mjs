dcbca68cbaaa8791fc03b8f01c1ee8ef1018de7c
craig <craig@craigs-iMac.local>
2020-08-13 11:43:16 +1000

    xxxx

--- modules/pwa.mjs
+++ modules/pwa.mjs
@@ -155,7 +155,7 @@ class PWA {
     fadeIn(element, duration) {
         (function increment(value = 0) {
             element.style.opacity = String(value);
-            if (element.style.opacity !== '1') {
+            if (Number(element.style.opacity) < 1) {
                 setTimeout(() => {
                     increment(value + 0.05);
                 }, duration / 20);
