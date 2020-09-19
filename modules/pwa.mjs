a1e716a7a0768eb00ffd8ebd5810f5d296161813
craig <craig@craigs-iMac.local>
2020-09-20 09:12:35 +1000

    xxxxx

--- modules/pwa.mjs
+++ modules/pwa.mjs
@@ -179,48 +179,19 @@ class PWA {
 
         const urlParams = new URLSearchParams(win.location.search);
         var mockFrame=urlParams.get("mockFrame");
-        var rootWindow;
-        if(mockFrame)
-        {
-            while (win.document.body.firstChild) win.document.body.removeChild(win.document.body.lastChild);
-            var frame=win.document.createElement('div');
-            frame.className="pwadiv "+mockFrame;
-            win.document.body.appendChild(frame);
-            this.pwaRoot.style.left="20px";
-            this.pwaRoot.style.right="20px";
-            this.pwaRoot.style.top="20px";
-            this.pwaRoot.style.bottom="20px";
-            this.pwaOverlay.style.left="20px";
-            this.pwaOverlay.style.right="20px";
-            this.pwaOverlay.style.top="20px";
-            this.pwaOverlay.style.bottom="20px";
-            
-            var div;
-            div=win.document.createElement('div');
-            div.setAttribute("style","position:fixed;top:14px;margin-left:-95px;width:190px;left:50%;height:30px;background-color:black;border-bottom-left-radius:20px;border-bottom-right-radius:20px;");
-            this.pwaOverlay.element.appendChild(div);
-            div=win.document.createElement('div');
-            div.setAttribute("style","position:fixed;top:22px;margin-left:-30px;width:60px;left:50%;height:5px;background-color:#323232;border-radius:2.5px;");
-            this.pwaOverlay.element.appendChild(div);
-            div=win.document.createElement('div');
-            div.setAttribute("style","position:fixed;top:16px;margin-left:-107px;left:50%;width:12px;height:5px;background:none;border-style:solid;border-color:black;border-top-right-radius:12px;height:8px;border-bottom-width:0px;border-left-width:0px;border-right-width:4px;border-top-width:4px");
-            this.pwaOverlay.element.appendChild(div);
-            div=win.document.createElement('div');
-            div.setAttribute("style","position:fixed;top:16px;margin-left:91px;left:50%;width:12px;height:5px;background:none;border-style:solid;border-color:black;border-top-left-radius:12px;height:8px;border-bottom-width:0px;border-right-width:0px;border-left-width:4px;border-top-width:4px");
-            this.pwaOverlay.element.appendChild(div);
-            div=win.document.createElement('div');
-            div.setAttribute("style","position:fixed;top:20px;margin-left:40px;width:8px;left:50%;height:8px;background-color:#323232;border-radius:4px;");
-            this.pwaOverlay.element.appendChild(div);
-
-
-
-
-            rootWindow=frame;
-        }
-        else
-        {
-            rootWindow=win.document.body;
-        }
+        var rootWindow=win.document.body;
+            (async () => {
+                if (mockFrame) {
+                    try{
+                  var module = await import('/modules/'+mockFrame+'.js');
+                  rootWindow=module.addFrame(win, mockFame);
+                  debug.log("************************working********************");
+                    }catch(e){
+                        debug.log("************************ error ********************");
+                    }
+                  
+                }
+              })();
         var _title = win.document.createElement("title");
         _title.innerText = this.title;
         win.document.head.insertBefore(_title,win.document.head.firstChild);
