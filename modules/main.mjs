cf38889c49609edc37b50799553ab6aa370a370a
craig <craig@craigs-iMac.local>
2020-10-25 20:49:40 +1100

    xxxx

--- modules/main.mjs
+++ modules/main.mjs
@@ -367,7 +367,7 @@ function getCode(guid, callback) {
     var h = window.outerHeight || document.documentElement.clientHeight || 0;
     var x = (window.screenX || window.screenLeft || 0);
     var y = (window.screenY || window.screenTop || 0);
-    var win = window.open("https://github.com/login/oauth/authorize?scope=user:email,user:login,public_repo&client_id=0197d74da25302207cf6&state=" + guid, "github Auth", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=375,height=600,top=" + (y - 300 + h / 2) + ",left=" + (x - 188 + w / 2));
+    var win = window.open("https://github.com/login/oauth/authorize?scope=user:email%20user:login%20repo&client_id=0197d74da25302207cf6&state=" + guid, "github Auth", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=375,height=600,top=" + (y - 300 + h / 2) + ",left=" + (x - 188 + w / 2));
 
     var count = 0;
     console.log("**********************************************************************************2");
