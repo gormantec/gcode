e3bdef4c5573dceedcc3ed0279cd9c68bc9ffae6
craig <craig@craigs-iMac.local>
2020-10-26 12:46:02 +1100

    xxxx

--- modules/main.mjs
+++ modules/main.mjs
@@ -636,7 +636,7 @@ function _toolbarButtonClicked() {
                                     try{
 
                                         console.log("get name");
-                                        githubtree.getGitFile(myLogin, "_repo", name, function (e, d) {
+                                        githubtree.getGitFile(myLogin, "_repo", "node_modules/ws/index.ts", function (e, d) {
                                             result=d;
                                             error=e;
                                             console.log("d="+d);
