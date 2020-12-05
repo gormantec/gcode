a5a94d5d1bb126c05f2fcd766635b9c4aaccc9d4
craig <craig@craigs-iMac.local>
2020-12-05 20:03:04 +1100

    xxxxx

--- modules/main.mjs
+++ modules/main.mjs
@@ -680,16 +680,27 @@ function _toolbarButtonClicked() {
                                             '  }';
 
                                     }
-                                    else if (name == "node_modules/wasmdom/package.json") {
+                                    else if (name == "node_modules/@wasmdom/package.json") {
                                         console.log(">>package.json");
                                         return '{\n' +
-                                            '    "name": "wasmdom",\n' +
+                                            '    "name": "@wasmdom",\n' +
                                             '   "version": "1.0.0",\n' +
                                             '   "description": "gormantec implementation of assembly script for DOM based PWA apps.",\n' +
                                             '   "dependencies": {\n' +
                                             '    },\n' +
                                             '    "private": true\n' +
                                             '  }';
+
+                                    }
+                                    else if (name.startsWith("node_modules/@wasmdom/assembly/")) {
+                                        console.log("got: "+name);
+                                        var b64 = localStorage.getItem("gitfile-git://gormantec:wasmdom/" + name.substring(22));
+                                        var cached = null;
+                                        if (b64) {
+                                            cached = atob(b64);
+                                            console.log(name.substring(22) + " = " + "git://gormantec:wasmdom/" + name.substring(22));
+                                        }
+                                        return cached;
 
                                     }
                                     else if (name == "asconfig.json") {
