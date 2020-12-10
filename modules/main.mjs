e9958cdf1a2522a9f9675c6c859585ea991391d2
craig <craig@craigs-iMac.local>
2020-12-11 05:50:57 +1100

    xxx

--- modules/main.mjs
+++ modules/main.mjs
@@ -706,7 +706,7 @@ function _toolbarButtonClicked() {
                                                 .then(response =>response.ok?response.text():null)
                                                 .then(text => {
                                                     console.log("downloaded:" + _name);
-                                                    if(text && text.indexOf("Page not found · GitHub Pages")<0)
+                                                    if(text)
                                                     {
                                                         localStorage.setItem("dist/" + _name.substring(pos), btoa(text));
                                                     }
