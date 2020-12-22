6d2e976f64fa05446f65ce5e783503a948f912e4
Craig Gorman <craig@gormantec.com>
2020-12-22 17:20:58 +1100

    xxxx

---
+++ modules/htmlUtils.mjs
@@ -0,0 +1,163 @@
+
+export function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+
+export function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+export function getTextColor(backColor) {
+
+    var backColor = backColor.substring(1);      // strip #
+    var rgb = parseInt(backColor, 16);   // convert rrggbb to decimal
+    var r = (rgb >> 16) & 0xff;  // extract red
+    var g = (rgb >> 8) & 0xff;  // extract green
+    var b = (rgb >> 0) & 0xff;  // extract blue
+    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+    if (luma < 124) {
+        // pick a different colour
+        return "#F0F0F0";
+    }
+    else {
+        return "#0F0F0F";
+    }
+}
+
+export function getImage(url, callback) {
+    if (!url || url.substring(url.length - 4) != ".png") {
+        callback({ error: "not a png file" });
+    }
+    else {
+        var arrayBufferToBase64 = function (buffer) {
+            var binary = '';
+            var bytes = [].slice.call(new Uint8Array(buffer));
+            bytes.forEach((b) => binary += String.fromCharCode(b));
+            return window.btoa(binary);
+        };
+        fetch(url, { mode: 'cors' }).then((response) => {
+            response.arrayBuffer().then((buffer) => {
+                var imageStr = arrayBufferToBase64(buffer);
+                callback(null, imageStr);
+            });
+        });
+    }
+
+}
+
+
+export function createHtml(code) {
+    var splashBackgroundColor = null;
+    var splash = null;
+    var mockFrame = null;
+    var rootHTML = window.document.createElement("html");
+    var rootHead = window.document.createElement("head");
+    var rootBody = window.document.createElement("body");
+    rootHTML.appendChild(rootHead);
+    rootHTML.appendChild(rootBody);
+    splash = code.replace(/\/\*.*?splash:.*?(http.*?[png|gif])[\n].*?\*\/.*/s, '$1');
+    if (splash == code) splash = null;
+    var icon = code.replace(/\/\*.*?icon:.*?(http.*?png)[\n].*?\*\/.*/s, '$1');
+    if (!icon || icon == code) icon = splash;
+    var icon180x180 = code.replace(/\/\*.*?icon180x180:.*?(http.*?png)[\n].*?\*\/.*/s, '$1');
+    if (!icon180x180 || icon180x180 == code) icon180x180 = icon;
+    debug.log("icon180x180=" + icon180x180);
+    var splashColor = code.replace(/\/\*.*?splashColor:.*?([A-Za-z0-9#]*)[\n].*?\*\/.*/s, '$1');
+    if (splashColor == code) splashColor = null;
+    debug.log("splashColor=" + splashColor);
+    mockFrame = code.replace(/\/\*.*?mockFrame:.*?([A-Za-z0-9#]*)[\n].*?\*\/.*/s, '$1');
+    if (mockFrame == code) mockFrame = null;
+    debug.log("mockFrame=" + mockFrame);
+    splashBackgroundColor = code.replace(/\/\*.*?splashBackgroundColor:.*?([A-Za-z0-9#]*)[\n].*?\*\/.*/s, '$1');
+    if (splashBackgroundColor == code) splashBackgroundColor = "black";
+    debug.log("splashBackgroundColor=" + splashBackgroundColor);
+    if (!splashColor && splashBackgroundColor) splashColor = getTextColor(splashBackgroundColor);
+    var splashDuration = code.replace(/\/\*.*?splashDuration:.*?([0-9]*)[\n].*?\*\/.*/s, '$1');
+    if (splashDuration == code) splashDuration = null;
+    var spinnerSize = code.replace(/\/\*.*?spinnerSize:.*?([A-Za-z0-9]*?).*?[\n].*?\*\/.*/s, '$1');
+    if (spinnerSize == code) spinnerSize = "50px";
+    var orientation = code.replace(/\/\*.*?orientation:.*?([A-Za-z0-9]*?).*?[\n].*?\*\/.*/s, '$1');
+    if (!orientation || orientation == code) orientation = "any";
+    var appName = code.replace(/\/\*.*?appName:.*?([A-Za-z0-9 ]*)[\n].*?\*\/.*/s, '$1');
+    if (!appName || appName == code) appName = "gcode App";
+    appName = appName.trim();
+    var manifest = code.replace(/\/\*.*?manifest:.*?(.*\.json)[\n].*?\*\/.*/s, '$1');
+    if (!manifest || manifest == "" || manifest == code) manifest = "xxxxx_manifest.json";
+    var longName = appName;
+    var shortName = appName;
+    var display = "standalone";
+    var _link = window.document.createElement("meta");
+    _link.setAttribute("name", "mobile-web-app-capable");
+    _link.setAttribute("content", "yes");
+    rootHead.appendChild(_link);
+    _link = window.document.createElement("meta");
+    _link.setAttribute("name", "apple-touch-fullscreen");
+    _link.setAttribute("content", "yes");
+    rootHead.appendChild(_link);
+    _link = window.document.createElement("meta");
+    _link.setAttribute("name", "apple-mobile-web-app-capable");
+    _link.setAttribute("content", "yes");
+    rootHead.appendChild(_link);
+    _link = window.document.createElement("meta");
+    _link.setAttribute("name", "apple-mobile-web-app-status-bar-style");
+    _link.setAttribute("content", "black-translucent");
+    rootHead.appendChild(_link);
+    _link = window.document.createElement("meta");
+    _link.setAttribute("property", "fpwa:template");
+    _link.setAttribute("content", "pwa=true,name=" + longName + ",short_name=" + shortName + ",theme_color=" + splashBackgroundColor + ",background_color=" + splashBackgroundColor + ",display=" + display + ",orientation=" + orientation);
+    rootHead.appendChild(_link);
+    _link = window.document.createElement("meta");
+    window.document.createElement("link");
+    _link.setAttribute("ref", "manifest");
+    _link.setAttribute("href", manifest);
+    rootHead.appendChild(_link);
+    _link = window.document.createElement("link");
+    _link.setAttribute("rel", "apple-touch-icon");
+    _link.setAttribute("href", "###ICONURI###");
+    _link = window.document.createElement("link");
+    _link.setAttribute("rel", "apple-touch-icon-precomposed");
+    _link.setAttribute("href", "###ICONURI###");
+    _link = window.document.createElement("link");
+    _link.setAttribute("rel", "icon");
+    _link.setAttribute("href", "###ICONURI###");
+
+
+
+
+    rootHead.appendChild(_link);
+    if (splash && splash.substring(0, 4) == "http" && splash.substring(splash.length - 3) == "png") {
+        var _style = window.document.createElement("style");
+        var spinnerCss = ".loader{font-size:" + spinnerSize + ";text-indent:-9999em;overflow:hidden;width:1em;height:1em;border-radius:50%;margin:72px auto;position:absolute;bottom:20px;left:20px;right:20px;bottom:0;-webkit-transform:translateZ(0);-ms-transform:translateZ(0);transform:translateZ(0);-webkit-animation:load6 1.7s infinite ease,round 1.7s infinite ease;animation:load6 1.7s infinite ease,round 1.7s infinite ease}@-webkit-keyframes load6{0%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}5%,95%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}10%,59%{box-shadow:0 -.83em 0 -.4em,-.087em -.825em 0 -.42em,-.173em -.812em 0 -.44em,-.256em -.789em 0 -.46em,-.297em -.775em 0 -.477em}20%{box-shadow:0 -.83em 0 -.4em,-.338em -.758em 0 -.42em,-.555em -.617em 0 -.44em,-.671em -.488em 0 -.46em,-.749em -.34em 0 -.477em}38%{box-shadow:0 -.83em 0 -.4em,-.377em -.74em 0 -.42em,-.645em -.522em 0 -.44em,-.775em -.297em 0 -.46em,-.82em -.09em 0 -.477em}100%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}}@keyframes load6{0%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}5%,95%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}10%,59%{box-shadow:0 -.83em 0 -.4em,-.087em -.825em 0 -.42em,-.173em -.812em 0 -.44em,-.256em -.789em 0 -.46em,-.297em -.775em 0 -.477em}20%{box-shadow:0 -.83em 0 -.4em,-.338em -.758em 0 -.42em,-.555em -.617em 0 -.44em,-.671em -.488em 0 -.46em,-.749em -.34em 0 -.477em}38%{box-shadow:0 -.83em 0 -.4em,-.377em -.74em 0 -.42em,-.645em -.522em 0 -.44em,-.775em -.297em 0 -.46em,-.82em -.09em 0 -.477em}100%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}}@-webkit-keyframes round{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}@keyframes round{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}";
+        _style.textContent = "\n.splash{position:absolute;background-size:contain;background-image:url(" + splash + ");background-position:center;background-repeat:no-repeat;top:0px;bottom:0px;left:0px;right:0px;" + (splashColor ? "color:" + splashColor : "") + ";" + (splashBackgroundColor ? "background-color:" + splashBackgroundColor : "") + ";}\n" + spinnerCss + "\n";
+        rootHead.appendChild(_style);
+    }
+
+    var _splash = window.document.createElement("div");
+    _splash.className = "splash";
+    rootBody.appendChild(_splash);
+    var _loader = window.document.createElement("div");
+    _loader.className = "loader";
+    _loader.innerText = "Loading...";
+    rootBody.appendChild(_loader);
+
+    var _script = window.document.createElement("script");
+    _script.text = "\n  window.PWA={globals:{}};\n";
+    if (appName) _script.text += "  window.PWA.globals.appName=\"" + appName + "\";\n";
+    if (orientation) _script.text += "  window.PWA.globals.orientation=\"" + orientation + "\";\n";
+    if (icon) _script.text += "  window.PWA.globals.icon=\"" + icon + "\";\n";
+    if (icon180x180) _script.text += "  window.PWA.globals.icon180x180=\"" + icon180x180 + "\";\n";
+    if (splash) _script.text += "  window.PWA.globals.splash=\"" + splash + "\";\n";
+    if (splashColor) _script.text += "  window.PWA.globals.splashColor=\"" + splashColor + "\";\n";
+    if (splashBackgroundColor) _script.text += "  window.PWA.globals.splashBackgroundColor=\"" + splashBackgroundColor + "\";\n";
+    if (splashDuration) _script.text += "  window.PWA.globals.splashDuration=" + parseInt(splashDuration) + ";\n";
+    rootHead.appendChild(_script);
+
+    return {"rootHTML":rootHTML,"splashBackgroundColor":splashBackgroundColor,"splash":splash,"mockFrame":mockFrame};
+}
--- modules/main.mjs
+++ modules/main.mjs
@@ -1,6 +1,7 @@
 
 import * as githubtree from '/modules/githubtree.mjs';
 import { beautify } from '/modules/beutify.mjs';
+import { htmlToElement,uuidv4,getImage,createHtml } from '/modules/htmlUtils.mjs';
 import dialogPolyfill from '/dist/dialog-polyfill/dialog-polyfill.esm.js';
 
 
@@ -28,21 +29,7 @@ var dirIconClosed = "keyboard_arrow_right";
 
 
 
-function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
-}
 
-// root functions
-
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
 
 
 function _save() {
@@ -374,22 +361,6 @@ function _onclickFilename() {
     };
 }
 
-function getTextColor(backColor) {
-
-    var backColor = backColor.substring(1);      // strip #
-    var rgb = parseInt(backColor, 16);   // convert rrggbb to decimal
-    var r = (rgb >> 16) & 0xff;  // extract red
-    var g = (rgb >> 8) & 0xff;  // extract green
-    var b = (rgb >> 0) & 0xff;  // extract blue
-    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
-    if (luma < 124) {
-        // pick a different colour
-        return "#F0F0F0";
-    }
-    else {
-        return "#0F0F0F";
-    }
-}
 
 function getCode(guid, callback) {
     var w = window.outerWidth || document.documentElement.clientWidth || 0;
@@ -419,26 +390,7 @@ function getCode(guid, callback) {
     }, 500);
 }
 
-function getImage(url, callback) {
-    if (!url || url.substring(url.length - 4) != ".png") {
-        callback({ error: "not a png file" });
-    }
-    else {
-        var arrayBufferToBase64 = function (buffer) {
-            var binary = '';
-            var bytes = [].slice.call(new Uint8Array(buffer));
-            bytes.forEach((b) => binary += String.fromCharCode(b));
-            return window.btoa(binary);
-        };
-        fetch(url, { mode: 'cors' }).then((response) => {
-            response.arrayBuffer().then((buffer) => {
-                var imageStr = arrayBufferToBase64(buffer);
-                callback(null, imageStr);
-            });
-        });
-    }
 
-}
 
 function _sms(params, callback) {
 
@@ -510,121 +462,8 @@ function _uploadFile(params, callback) {
     //encodedicon ###ICONURI###
     //https://s3-ap-southeast-2.amazonaws.com/fpwa.web.gormantec.com/apps/5ojnj1pknl.html
 }
-var splashBackgroundColor = null;
-var splash = null;
-var mockFrame = null;
-function _createHtml() {
-    splashBackgroundColor = null;
-    splash = null;
-    mockFrame = null;
-    var rootHTML = window.document.createElement("html");
-    var rootHead = window.document.createElement("head");
-    var rootBody = window.document.createElement("body");
-    rootHTML.appendChild(rootHead);
-    rootHTML.appendChild(rootBody);
-    var code = editor.getValue();
-    splash = code.replace(/\/\*.*?splash:.*?(http.*?[png|gif])[\n].*?\*\/.*/s, '$1');
-    if (splash == code) splash = null;
-    var icon = code.replace(/\/\*.*?icon:.*?(http.*?png)[\n].*?\*\/.*/s, '$1');
-    if (!icon || icon == code) icon = splash;
-    var icon180x180 = code.replace(/\/\*.*?icon180x180:.*?(http.*?png)[\n].*?\*\/.*/s, '$1');
-    if (!icon180x180 || icon180x180 == code) icon180x180 = icon;
-    debug.log("icon180x180=" + icon180x180);
-    var splashColor = code.replace(/\/\*.*?splashColor:.*?([A-Za-z0-9#]*)[\n].*?\*\/.*/s, '$1');
-    if (splashColor == code) splashColor = null;
-    debug.log("splashColor=" + splashColor);
-    mockFrame = code.replace(/\/\*.*?mockFrame:.*?([A-Za-z0-9#]*)[\n].*?\*\/.*/s, '$1');
-    if (mockFrame == code) mockFrame = null;
-    debug.log("mockFrame=" + mockFrame);
-    splashBackgroundColor = code.replace(/\/\*.*?splashBackgroundColor:.*?([A-Za-z0-9#]*)[\n].*?\*\/.*/s, '$1');
-    if (splashBackgroundColor == code) splashBackgroundColor = "black";
-    debug.log("splashBackgroundColor=" + splashBackgroundColor);
-    if (!splashColor && splashBackgroundColor) splashColor = getTextColor(splashBackgroundColor);
-    var splashDuration = code.replace(/\/\*.*?splashDuration:.*?([0-9]*)[\n].*?\*\/.*/s, '$1');
-    if (splashDuration == code) splashDuration = null;
-    var spinnerSize = code.replace(/\/\*.*?spinnerSize:.*?([A-Za-z0-9]*?).*?[\n].*?\*\/.*/s, '$1');
-    if (spinnerSize == code) spinnerSize = "50px";
-    var orientation = code.replace(/\/\*.*?orientation:.*?([A-Za-z0-9]*?).*?[\n].*?\*\/.*/s, '$1');
-    if (!orientation || orientation == code) orientation = "any";
-    var appName = code.replace(/\/\*.*?appName:.*?([A-Za-z0-9 ]*)[\n].*?\*\/.*/s, '$1');
-    if (!appName || appName == code) appName = "gcode App";
-    appName = appName.trim();
-    var manifest = code.replace(/\/\*.*?manifest:.*?(.*\.json)[\n].*?\*\/.*/s, '$1');
-    if (!manifest || manifest == "" || manifest == code) manifest = "xxxxx_manifest.json";
-    var longName = appName;
-    var shortName = appName;
-    var display = "standalone";
 
 
-
-    var _link = window.document.createElement("meta");
-    _link.setAttribute("name", "mobile-web-app-capable");
-    _link.setAttribute("content", "yes");
-    rootHead.appendChild(_link);
-    _link = window.document.createElement("meta");
-    _link.setAttribute("name", "apple-touch-fullscreen");
-    _link.setAttribute("content", "yes");
-    rootHead.appendChild(_link);
-    _link = window.document.createElement("meta");
-    _link.setAttribute("name", "apple-mobile-web-app-capable");
-    _link.setAttribute("content", "yes");
-    rootHead.appendChild(_link);
-    _link = window.document.createElement("meta");
-    _link.setAttribute("name", "apple-mobile-web-app-status-bar-style");
-    _link.setAttribute("content", "black-translucent");
-    rootHead.appendChild(_link);
-    _link = window.document.createElement("meta");
-    _link.setAttribute("property", "fpwa:template");
-    _link.setAttribute("content", "pwa=true,name=" + longName + ",short_name=" + shortName + ",theme_color=" + splashBackgroundColor + ",background_color=" + splashBackgroundColor + ",display=" + display + ",orientation=" + orientation);
-    rootHead.appendChild(_link);
-    _link = window.document.createElement("meta");
-    window.document.createElement("link");
-    _link.setAttribute("ref", "manifest");
-    _link.setAttribute("href", manifest);
-    rootHead.appendChild(_link);
-    _link = window.document.createElement("link");
-    _link.setAttribute("rel", "apple-touch-icon");
-    _link.setAttribute("href", "###ICONURI###");
-    _link = window.document.createElement("link");
-    _link.setAttribute("rel", "apple-touch-icon-precomposed");
-    _link.setAttribute("href", "###ICONURI###");
-    _link = window.document.createElement("link");
-    _link.setAttribute("rel", "icon");
-    _link.setAttribute("href", "###ICONURI###");
-
-
-
-
-    rootHead.appendChild(_link);
-    if (splash && splash.substring(0, 4) == "http" && splash.substring(splash.length - 3) == "png") {
-        var _style = window.document.createElement("style");
-        var spinnerCss = ".loader{font-size:" + spinnerSize + ";text-indent:-9999em;overflow:hidden;width:1em;height:1em;border-radius:50%;margin:72px auto;position:absolute;bottom:20px;left:20px;right:20px;bottom:0;-webkit-transform:translateZ(0);-ms-transform:translateZ(0);transform:translateZ(0);-webkit-animation:load6 1.7s infinite ease,round 1.7s infinite ease;animation:load6 1.7s infinite ease,round 1.7s infinite ease}@-webkit-keyframes load6{0%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}5%,95%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}10%,59%{box-shadow:0 -.83em 0 -.4em,-.087em -.825em 0 -.42em,-.173em -.812em 0 -.44em,-.256em -.789em 0 -.46em,-.297em -.775em 0 -.477em}20%{box-shadow:0 -.83em 0 -.4em,-.338em -.758em 0 -.42em,-.555em -.617em 0 -.44em,-.671em -.488em 0 -.46em,-.749em -.34em 0 -.477em}38%{box-shadow:0 -.83em 0 -.4em,-.377em -.74em 0 -.42em,-.645em -.522em 0 -.44em,-.775em -.297em 0 -.46em,-.82em -.09em 0 -.477em}100%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}}@keyframes load6{0%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}5%,95%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}10%,59%{box-shadow:0 -.83em 0 -.4em,-.087em -.825em 0 -.42em,-.173em -.812em 0 -.44em,-.256em -.789em 0 -.46em,-.297em -.775em 0 -.477em}20%{box-shadow:0 -.83em 0 -.4em,-.338em -.758em 0 -.42em,-.555em -.617em 0 -.44em,-.671em -.488em 0 -.46em,-.749em -.34em 0 -.477em}38%{box-shadow:0 -.83em 0 -.4em,-.377em -.74em 0 -.42em,-.645em -.522em 0 -.44em,-.775em -.297em 0 -.46em,-.82em -.09em 0 -.477em}100%{box-shadow:0 -.83em 0 -.4em,0 -.83em 0 -.42em,0 -.83em 0 -.44em,0 -.83em 0 -.46em,0 -.83em 0 -.477em}}@-webkit-keyframes round{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}@keyframes round{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}";
-        _style.textContent = "\n.splash{position:absolute;background-size:contain;background-image:url(" + splash + ");background-position:center;background-repeat:no-repeat;top:0px;bottom:0px;left:0px;right:0px;" + (splashColor ? "color:" + splashColor : "") + ";" + (splashBackgroundColor ? "background-color:" + splashBackgroundColor : "") + ";}\n" + spinnerCss + "\n";
-        rootHead.appendChild(_style);
-    }
-
-    var _splash = window.document.createElement("div");
-    _splash.className = "splash";
-    rootBody.appendChild(_splash);
-    var _loader = window.document.createElement("div");
-    _loader.className = "loader";
-    _loader.innerText = "Loading...";
-    rootBody.appendChild(_loader);
-
-    var _script = window.document.createElement("script");
-    _script.text = "\n  window.PWA={globals:{}};\n";
-    if (appName) _script.text += "  window.PWA.globals.appName=\"" + appName + "\";\n";
-    if (orientation) _script.text += "  window.PWA.globals.orientation=\"" + orientation + "\";\n";
-    if (icon) _script.text += "  window.PWA.globals.icon=\"" + icon + "\";\n";
-    if (icon180x180) _script.text += "  window.PWA.globals.icon180x180=\"" + icon180x180 + "\";\n";
-    if (splash) _script.text += "  window.PWA.globals.splash=\"" + splash + "\";\n";
-    if (splashColor) _script.text += "  window.PWA.globals.splashColor=\"" + splashColor + "\";\n";
-    if (splashBackgroundColor) _script.text += "  window.PWA.globals.splashBackgroundColor=\"" + splashBackgroundColor + "\";\n";
-    if (splashDuration) _script.text += "  window.PWA.globals.splashDuration=" + parseInt(splashDuration) + ";\n";
-    rootHead.appendChild(_script);
-
-    return rootHTML;
-}
 
 var win;
 var myLogin = "";
@@ -687,15 +526,20 @@ function _toolbarButtonClicked() {
         else if (filename.endsWith(".ts")) {
             debug.log(myLogin + "$ asc " + filename + " --target release\n");
             import('/modules/ascompile.mjs').then(({ run }) => {
+                var code=editor.getValue();
                 run(
-                    editor.getValue(),
+                    code,
                     "node_modules/wasmdom/assembly/index.ts",
                     "wasmdom/assembly/src/app.ts",
                     "optimized.wasm",
                     (d) => {
                         console.log(d);
                         try {
-                            var rootHTML = _createHtml();
+                            var result = createHtml(code);
+                            var splashBackgroundColor=result.splashBackgroundColor;
+                            var splash=result.splash;
+                            var mockFrame=result.mockFrame;
+                            var rootHTML=result.rootHTML;
                             var _script1 = window.document.createElement("script");
                             _script1.text = "\nwindow.wasmdomURL=\"" + dataURL + "\";\n";
                             rootHTML.querySelector("body").appendChild(_script1);
@@ -747,7 +591,13 @@ function _toolbarButtonClicked() {
         else if (filename.endsWith(".mjs")) {
             debug.log(myLogin + "$ launch webApp " + filename + "\n");
             try {
-                var rootHTML = _createHtml();
+                var code=editor.getValue();
+                var result = createHtml(code);
+                var splashBackgroundColor=result.splashBackgroundColor;
+                var splash=result.splash;
+                var mockFrame=result.mockFrame;
+                var rootHTML=result.rootHTML;
+
                 var _module = window.document.createElement("script");
                 _module.setAttribute("type", "module");
                 _module.text = "\n" + editor.getValue() + "\n";
