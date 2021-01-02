0b9d63583417e5773d1bd494cf701eaf2ae76df0
Craig Gorman <craig@gormantec.com>
2021-01-03 07:34:21 +1100

    xxxxx

--- modules/features/addFeature.mjs
+++ modules/features/addFeature.mjs
@@ -1,13 +1,9 @@
 /* Feature Name: Add Feature */
 
-export function menuMetadata(){
-
-    return {"id":"addFeatureButton","class":"pageLeftToolbarButton","materialIcon":"add_circle_outline"};
-
-}
+export const menuMetadata = {"id":"addFeatureButton","class":"pageLeftToolbarButton","materialIcon":"add_circle_outline"};
 
 export function menuAction(){
-    return function(){
-        prompt("Add feature: "); ;
-    };
+
+        prompt("Add feature: ");
+        
 }
--- modules/features/helpMenu.mjs
+++ modules/features/helpMenu.mjs
@@ -1,13 +1,7 @@
 /* Feature Name: Help Menu */
 
-export function menuMetadata(){
-
-    return {"id":"getHelp","class":"pageLeftToolbarButton","materialIcon":"live_help"};
-
-}
+export const menuMetadata =  {"id":"getHelp","class":"pageLeftToolbarButton","materialIcon":"live_help"};
 
 export function menuAction(){
-    return function(){
-
-    };
+    console.log("Help");
 }
