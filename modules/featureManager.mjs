eecfa7bbf559c41a88ca42e150e43bc3f14865c9
Craig Gorman <craig@gormantec.com>
2021-01-04 07:55:33 +1100

    xxx

--- modules/featureManager.mjs
+++ modules/featureManager.mjs
@@ -38,7 +38,8 @@ export async function loadFeatures() {
                         console.log("Added: "+meta.id + " to  "+pageLeftToolbar);
 
                         if (isFunction(menuAction)) {
-                            consile.log((new Function(";("+menuAction.toString()+")();").toString());
+                            console.log((newFunction(";("+menuAction.toString()+")();")).toString());
+
                             document.getElementById(meta.id).onclick = new Function(";("+menuAction.toString()+")();");
                         }
                     }
