6846e0ee3d4f507e7cc1fd093f81ed47f180bb97
Craig Gorman <craig@gormantec.com>
2021-02-13 23:33:10 +1100

    xxx

--- modules/main.mjs
+++ modules/main.mjs
@@ -424,7 +424,7 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         foldGutter: true,
         gutters: ["CodeMirror-lint-markers", "CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-        lint: { 'esversion': 6 }
+        lint: { 'esversion': '6' }
     });
 
     Array.from(document.getElementsByClassName("cm-s-theme")).forEach(function (e) { e.classList.add('cm-s-' + theme); });
