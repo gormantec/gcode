f58c50bc1a01541ba81c80ec3b7467390d2aa7e4
Craig Gorman <craig@gormantec.com>
2021-05-24 20:42:28 +1000

    xxx

--- modules/ascompile.mjs
+++ modules/ascompile.mjs
@@ -187,8 +187,8 @@ function createDownload(blobname,blob)
     var blobUrl = URL.createObjectURL(blob);
 
     var save = document.createElement('a');
-    save.style.marginRight ="20px";
-    save.style.marginTop ="20px";
+    save.style.marginLeft ="-80px";
+    save.style.marginTop ="10px";
     save.style.color ="white";
     save.href = blobUrl;
     save.download = blobname;
