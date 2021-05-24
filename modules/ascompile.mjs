496988ec8fd6c04af989c50663c27480f95feb61
Craig Gorman <craig@gormantec.com>
2021-05-24 20:22:29 +1000

    xxxx

--- modules/ascompile.mjs
+++ modules/ascompile.mjs
@@ -91,6 +91,9 @@ export function run(sourceCode, mainFilename, editorFilename, outputFilename, da
                                     console.log("write load: "+(typeof reader.result));
                                     dataURL = reader.result;
                                 }, false);
+
+                                createDownload(new Blob([Uint8Array.from(data)], { type: 'application/wasm' }));
+                                
                                 reader.readAsDataURL(new Blob([Uint8Array.from(data)], { type: 'application/wasm' }));
                             }
                         },
@@ -154,4 +157,45 @@ export function run(sourceCode, mainFilename, editorFilename, outputFilename, da
         _run();
     }
     catch (e) { window.debug.log(e); }
+}
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {
+        type: mimeString
+    });
+}
+
+function createDownload(blob)
+{
+    //var blob = dataURItoBlob(message.content);
+
+    var blobUrl = URL.createObjectURL(blob);
+
+    var save = document.createElement('a'),
+        event;
+    save.href = blobUrl;
+    save.download = message.identifier;
+
+    event = document.createEvent('Event');
+    event.initEvent('click', true, true);
+
+    save.dispatchEvent(event);
+    (window.URL || window.webkitURL).revokeObjectURL(save.href);
+
 }
