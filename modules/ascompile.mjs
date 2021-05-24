af9b69ffabed6d028121ec042bac50b009b1b077
Craig Gorman <craig@gormantec.com>
2021-05-24 20:15:25 +1000

    ddd

--- modules/ascompile.mjs
+++ modules/ascompile.mjs
@@ -16,7 +16,7 @@ export function run(sourceCode, mainFilename, editorFilename, outputFilename, da
                     const stderr = asc.createMemoryStream();
                     asc.main([
                         mainFilename,
-                        "--binaryFile", "myModule.wasm",
+                        "--binaryFile", outputFilename,
                     ], {
                         stdout,
                         stderr,
