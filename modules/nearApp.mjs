08cbeb1b9258208ff20699dffcd86dce3180403a
Craig Gorman <craig@gormantec.com>
2021-05-26 15:32:42 +1000

    xxx

--- modules/nearApp.mjs
+++ modules/nearApp.mjs
@@ -2,59 +2,49 @@ export function upload() {
 
 }
 
+async function doNear(nearApi) {
+    const near = new nearApi.Near({
+        keyStore: keyStore,
+        networkId: 'testnet',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        walletUrl: 'https://wallet.testnet.near.org'
+    });
+
+
+    console.log(near);
+    const wallet = new nearApi.WalletConnection(near);
+    //const account = await near.account("gormantec.testnet");
+    if (!wallet.isSignedIn()) {
+        wallet.requestSignIn("gormantec.testnet");
+    }
+    console.log(wallet);
+
+    const contract = new nearApi.Contract(
+        wallet.account(), // the account object that is connecting
+        "dev-1621939186587-7091001",
+        {
+            // name of contract you're connecting to
+            viewMethods: ["getGreeting"], // view methods do not change state but usually return a value
+            changeMethods: ["setGreeting"], // change methods modify state
+        }
+    );
+
+    await contract.setGreeting(
+        {
+            message: "hello.10", // argument name and value - pass empty object if no args required
+        }
+    );
+
+}
+
+
+
 export function test() {
     console.log("TEST1!");
     require(["https://cdn.jsdelivr.net/npm/near-api-js@0.41.0/dist/near-api-js.min.js"], async ({ nearApi }) => {
 
         console.log("TEST2!");
-        const keyStore = new nearApi.keyStores.BrowserLocalStorageKeyStore();
-
-        const near = new nearApi.Near({
-            keyStore: keyStore,
-            networkId: 'testnet',
-            nodeUrl: 'https://rpc.testnet.near.org',
-            walletUrl: 'https://wallet.testnet.near.org'
-        });
-
-
-        /*
-        const keyPair = nearApi.KeyPair.fromRandom("ed25519");
-        const publicKey = keyPair.publicKey.toString();
-        await keyStore.setKey("testnet", publicKey, keyPair);
-
-        const account = await near.account("hello.gormantec.testnet");
-        await account.addKey(
-            publicKey, // public key for new account
-        );
-        */
-
-
-        const wallet = new nearApi.WalletConnection(near);
-        //const account = await near.account("gormantec.testnet");
-        if (!wallet.isSignedIn()) {
-            console.log("wallet not signed");
-            wallet.requestSignIn("gormantec.testnet");
-        }
-
-        console.log("isSignedIn:"+wallet.isSignedIn());
-        const contract = new nearApi.Contract(
-            wallet.account(), // the account object that is connecting
-            "dev-1621939186587-7091001",
-            {
-                // name of contract you're connecting to
-                viewMethods: ["getGreeting"], // view methods do not change state but usually return a value
-                changeMethods: ["setGreeting"], // change methods modify state
-            }
-        );
-        
-        console.log("contract");
-        console.log(contract);
-
-        await contract.setGreeting(
-            {
-                message: "hello.10", // argument name and value - pass empty object if no args required
-            }
-        );
+        doNear(nearApi);
 
     });
 }
