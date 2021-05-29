5c17632d96b44afd8264c8df7ec2058ef74afae1
Craig Gorman <craig@gormantec.com>
2021-05-29 11:17:31 +1000

    xxx

--- modules/sample.dapp.ts
+++ modules/sample.dapp.ts
@@ -3,25 +3,21 @@ import { Context, logging, storage } from 'near-sdk-as'
 
 const DEFAULT_MESSAGE = 'Hello'
 
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
+/** 
+ * @Method("getGreeting");
+ * @testing({"name":"can get greeting","test":{"accountId":"accountId"},"result":"hello"});
+*/
 export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/roles/developer/contracts/assemblyscript#imports
   return storage.get<string>(accountId, DEFAULT_MESSAGE)
 }
 
+
+/** 
+ * @Method("setGreeting");
+ * @testing({"name":"can set greeting","test":{"message":"hello"},"result":"null"});
+*/
 export function setGreeting(message: string): void {
   const account_id = Context.sender
-
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(
-    // String interpolation (`like ${this}`) is a work in progress:
-    // https://github.com/AssemblyScript/assemblyscript/pull/1115
-    'Saving greeting "' + message + '" for account "' + account_id + '"'
-  )
-
+  logging.log( 'Saving greeting "' + message + '" for account "' + account_id + '"')
   storage.set(account_id, message)
 }
