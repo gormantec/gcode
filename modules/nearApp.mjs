a11f6c3128f888e259afe61ec62bc8869c3f6787
Craig Gorman <craig@gormantec.com>
2021-06-08 19:21:22 +1000

    xxx

--- modules/nearApp.mjs
+++ modules/nearApp.mjs
@@ -34,7 +34,8 @@ function nearConfig(nearApi)
         keyStore: keyStore,
         networkId: 'testnet',
         nodeUrl: 'https://rpc.testnet.near.org',
-        walletUrl: 'https://wallet.testnet.near.org'
+        walletUrl: 'https://wallet.testnet.near.org',
+        helperUrl: 'https://helper.testnet.near.org'
     };
 }
 export async function login(config) {
