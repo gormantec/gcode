40a17a2eb693e62fe76916f5a1ebb0302fa21065
Craig Gorman <craig@gormantec.com>
2021-06-12 18:09:13 +1000

    xxx

--- modules/features/nearMenu.mjs
+++ modules/features/nearMenu.mjs
@@ -5,5 +5,8 @@ import { remove } from '/modules/near/index.mjs';
 export const menuMetadata =  {"id":"removeNear","class":"pageLeftToolbarButton","materialIcon":"delete_forever"};
 
 export function menuAction(){
-    console.log("Help");
+    var sourceCode=window.editor.getValue();
+    var accountId = sourceCode.replace(/^[\s\S]*?@Near.*?"accountId".*?"(.*?)"[\s\S]*$/, "$1");
+    var contractId = sourceCode.replace(/^[\s\S]*?@Near.*?"contractId".*?"(.*?)"[\s\S]*$/, "$1");
+    remove({ accountId: accountId, contractId: contractId });
 }
