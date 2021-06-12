e8aecb9fc32d9133db9b62fc9e16c8e19d68ed2f
Craig Gorman <craig@gormantec.com>
2021-06-13 08:22:38 +1000

    sss

--- modules/near/compiler.mjs
+++ modules/near/compiler.mjs
@@ -4,7 +4,7 @@ import { nearConfig } from './nearConfig.mjs';
 
 const getNearApi = getScript('https://cdn.jsdelivr.net/npm/near-api-js@0.41.0/dist/near-api-js.min.js', ["nearApi"]);
 const getAWS = getScript('https://sdk.amazonaws.com/js/aws-sdk-2.918.0.min.js', ["AWS"]);
-const getJSZip = getScript('https:gcode.com.au/js/jszip.min.js', ["JSZip"]);
+const getJSZip = getScript('https://gcode.com.au/js/jszip.min.js', ["JSZip"]);
 
 
 
