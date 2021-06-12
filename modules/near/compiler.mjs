4d1253c8827c3c74f4832c2fef5abbc0fd2cc733
Craig Gorman <craig@gormantec.com>
2021-06-13 09:06:23 +1000

    cccc

--- modules/near/compiler.mjs
+++ modules/near/compiler.mjs
@@ -106,7 +106,7 @@ async function doNear(nearApi, config) {
                 var modP=list[i].parameters;
                 for (const key in modP) {
                     if (modP.hasOwnProperty(key)) {
-                        if(user[key].startsWith("@Near." && config[user[key].substring(6)]))modP[key]=config[user[key].substring(6)];
+                        if(modP[key].startsWith("@Near." && config[modP[key].substring(6)]))modP[key]=config[modP[key].substring(6)];
                     }
                 }
                 mycontract[list[i].method](list[i].parameters).then((r) => {
