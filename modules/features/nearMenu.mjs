032254d4c196b547afd8293e5a33e960fee393ec
Craig Gorman <craig@gormantec.com>
2021-06-12 18:50:49 +1000

    xxxx

--- modules/features/nearMenu.mjs
+++ modules/features/nearMenu.mjs
@@ -7,11 +7,6 @@ export const menuMetadata =  {"id":"removeNear","class":"pageLeftToolbarButton",
 export function menuAction(){
 
     document.getElementById("newFileDialog").showModal();
-
-    var sourceCode=window.editor.getValue();
-    var accountId = sourceCode.replace(/^[\s\S]*?@Near.*?"accountId".*?"(.*?)"[\s\S]*$/, "$1");
-    var contractId = sourceCode.replace(/^[\s\S]*?@Near.*?"contractId".*?"(.*?)"[\s\S]*$/, "$1");
-    remove({ accountId: accountId, contractId: contractId });
 }
 
 
@@ -29,4 +24,16 @@ export const dialogMetadata = [
         "ok": {  "value": "remove" }
     }
 ];
+
+export function dialogAction(event) {
+    if ( event.type=="dialog" && event.id=="nearDialog" && event.value == "remove") {
+        var sourceCode=window.editor.getValue();
+        var accountId = sourceCode.replace(/^[\s\S]*?@Near.*?"accountId".*?"(.*?)"[\s\S]*$/, "$1");
+        var contractId = sourceCode.replace(/^[\s\S]*?@Near.*?"contractId".*?"(.*?)"[\s\S]*$/, "$1");
+        remove({ accountId: accountId, contractId: contractId });
+    }
+    else if(event.type=="select" && event.id=="nearDialogSelect")
+    {
+    }
+}
 
