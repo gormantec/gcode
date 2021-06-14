af02a502d3515fb994682bdadf70bb98c6b05c89
Craig Gorman <craig@gormantec.com>
2021-06-14 14:31:55 +1000

    xxxx

--- modules/pwa.mjs
+++ modules/pwa.mjs
@@ -342,13 +342,13 @@ class Div {
         }
         if (params && params.width) {
             this.element.style.width = params.width;
-            if (!params.right) this.element.style.right="";
-            if (!params.left) this.element.style.left="";
+            if (!params.right) this.element.style.right="unset";
+            if (!params.left) this.element.style.left="unset";
         }
         if (params && params.height) {
             this.element.style.height = params.height;
-            if (!params.top) this.element.style.top="";
-            if (!params.bottom) this.element.style.bottom="";
+            if (!params.top) this.element.style.top="unset";
+            if (!params.bottom) this.element.style.bottom="unset";
         }
         if (params && params.backgroundColor) this.element.style.backgroundColor = params.backgroundColor;
         if (params && params.backgroundPosition) this.element.style.backgroundPosition = params.backgroundPosition;
