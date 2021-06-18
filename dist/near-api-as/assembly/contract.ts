19e9ea5dd4487c4cc1e65dab9371644abfc6956c
Craig Gorman <craig@gormantec.com>
2021-06-18 16:49:38 +1000

    xxx

--- dist/near-api-as/assembly/contract.ts
+++ dist/near-api-as/assembly/contract.ts
@@ -1,9 +1,10 @@
 import { Account } from "./account";
+import {Window} from "../../wasmdom/assembly/dom";
 
 export class Contract{
     constructor(account:Account,contractId:string,methods:ContractMethods)
     {
-
+        Window.window.console.log(methods.viewMethods[0]);
     }
 
 }
