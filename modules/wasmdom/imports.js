705cd317dc59cd5ad883467bbd5aabc6ec9fb366
Craig Gorman <craig@gormantec.com>
2021-06-20 08:48:02 +1000

    xxxx

--- modules/wasmdom/imports.js
+++ modules/wasmdom/imports.js
@@ -208,10 +208,14 @@ export function init(window,_fetch,_Response) {
         if(!b || b=="" || b.trim().substring(0,1)!="{")
         {
           var p= getPointer(fetch(u));
+
+          console.log("fetch Pointer="+p);
           return p;
         }
         else {
           var p= getPointer(fetch(u,{method:m,headers:JSON.parse(h),body:b}));
+
+          console.log("fetch Pointer="+p);
           return p;
         }
 
@@ -225,11 +229,11 @@ export function init(window,_fetch,_Response) {
           if(res instanceof Response)
           {
             var r= getPointer(res);
-            console.log("Pointer="+p+ " Response="+r);
+            console.log("fetch Pointer="+p+ " Response="+r);
             _wasm.__alertPromise(p,r);
           }
           else {
-            console.log("Pointer="+p+ " Response="+r);
+            console.log("fetch Pointer="+p+ " Response="+r);
             _wasm.__alertPromiseText(p,_wasm.__pin(_wasm.__newString(res.toString())));
           }
         });
