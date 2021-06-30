3033a9e639b25856a26fd3c81595a479a9957774
Craig Gorman <craig@gormantec.com>
2021-06-30 17:30:10 +1000

    xxx

--- modules/ascompile.mjs
+++ modules/ascompile.mjs
@@ -87,7 +87,7 @@ export function run(sourceCode, mainFilename, editorFilename, outputFilename, da
                                         stderr,
                                         readFile(name, baseDir) {
 
-                                            if(name.indexOf("wasmdom-globals")>=0)console.log(name);
+                                            if(name.indexOf("wasmdom-globals")>=0)console.log("...."+name);
                                             
                                             const _fileData = load(name, true);
                                             if (baseDir == "." && _fileData && name.indexOf("node_modules") < 0) {
