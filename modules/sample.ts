655a675076cd175c1159323c353946d8dc09433c
Craig Gorman <craig@gormantec.com>
2021-07-02 10:53:15 +1000

    xxxx

--- modules/sample.ts
+++ modules/sample.ts
@@ -35,44 +35,58 @@ export function run(w: Window, d: Document, c: Console): i32 {
     });
 
     aPWA.show(window);
-    console.log('PWA Loaded');
 
     if (aPWA.floatingActionButton) {
-        var d1: Div = <Div>aPWA.floatingActionButton;
-        d1.onclick(function () {
-            console.log("Opening a new page");
+        var d1: Div = < Div > aPWA.floatingActionButton;
+        d1.onclick(function() {
+            Debug.log("Opening a new page");
             aPWA.setPage(secondPage);
         });
     }
 
     if (aPWA.pwaBody) {
-        var d2: Div = <Div>aPWA.pwaBody;
+        var d2: Div = < Div > aPWA.pwaBody;
         d2.style.backgroundColor = "black";
     }
 
-    window.setTimeout(function () {
+    window.setTimeout(function() {
         Debug.log("Homepage: loaded");
         aPWA.setPage(homePage);
     }, 1000);
 
-    window.setTimeout(function () {
-        Debug.log(">>NEAR: contract executing");
-        var aKeyStore = new BrowserLocalStorageKeyStore();
-        const config = new NearConfig(aKeyStore, "testnet", "hello.gormantec.testnet");
-        const near = new Near(config);
-        const account = new Account(near.connection, "hello.gormantec.testnet");
-        const mycontract = new Contract(account, "hello.gormantec.testnet", { viewMethods: ["getGreeting"] });
-        mycontract.exec({ methodName: "getGreeting", paramaters: '{"accountId":"hello.gormantec.testnet"}' }).thenString((text:string)=>{
-            Debug.log(">>NEAR: result:"+text);
-            Debug.log(">>NEAR: text:"+Contract.decodeResult(text));
-            return null;
-
-        });
-        Debug.log(">>NEAR: Contract");
-    }, 5000);
-
-
-
+    callContract(2000);
     return 0;
 
+}
+
+function callContract(delay: i32): void {
+    window.setTimeout(function() {
+        var accountId = "hello.gormantec.testnet";
+        setBlackPage("<p> Contract Request: " + accountId + "</p>");
+        var aKeyStore = new BrowserLocalStorageKeyStore();
+        const config = new NearConfig(aKeyStore, "testnet", accountId);
+        const near = new Near(config);
+        const account = new Account(near.connection, accountId);
+        const mycontract = new Contract(account, accountId, {
+            viewMethods: ["getGreeting"]
+        });
+        mycontract.exec({
+                methodName: "getGreeting",
+                paramaters: '{"accountId":"' + accountId + '"}'
+            })
+            .thenString((text: string) => {
+                text = Contract.decodeResult(text);
+                setBlackPage("<p> Contract Response: " + text + "</p>");
+                return null;
+            });
+    }, delay);
+}
+
+function setBlackPage(html: string): void {
+    aPWA.setPage(new Page( < PageParams > {
+        navigateBackPage: homePage,
+        child: new Div( < DivParams > {
+            innerHTML: html
+        })
+    }));
 }
