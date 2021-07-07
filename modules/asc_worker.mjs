4afe1a49e45886a85bd5036c8e70745abc70d8ff
Craig Gorman <craig@gormantec.com>
2021-07-07 13:19:56 +1000

    xxx

--- modules/asc_worker.mjs
+++ modules/asc_worker.mjs
@@ -1,66 +1,12 @@
-const getRequire = getScript('https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js', ["require"]);
+importScripts("https://cdn.jsdelivr.net/npm/assemblyscript@latest/dist/sdk.js");
+
+console.log(asc);
 
 onmessage = function (e) {
 
     console.log("worker 1: "+e.data);
-    getRequire.then(({ require }) => {
-        require(["https://cdn.jsdelivr.net/npm/assemblyscript@latest/dist/sdk.js"], ({ asc }) => {
-            console.log("worker 2");
-        });
-    });
+
 }
 
 
-/** FUNCTIONS */
-async function getScript(src, objects) {
-    return new Promise((resolve, reject) => {
-        var script = window.document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = src;
-        script.async = 'true';
-        script.id = cyrb53(src);
-        var exists = document.getElementById(script.id);
-        if (exists) {
-            if (resolved[script.id]) {
-                resolve(resolved[script.id]);
-            }
-            else {
-                document.getElementById(script.id).addEventListener('load', function () {
-                    var o = {};
-                    if (objects && objects.length > 0) {
-                        objects.forEach((o1) => {
-                            o[o1] = window[o1];
-                        })
-                    }
-                    resolved[script.id] = o;
-                    resolve(o);
-                });
-            }
-        }
-        else {
-            document.getElementsByTagName('head')[0].appendChild(script);
-            script.addEventListener('load', function () {
-                var o = {};
-                if (objects && objects.length > 0) {
-                    objects.forEach((o1) => {
-                        o[o1] = window[o1];
-                    })
-                }
-                resolved[script.id] = o;
-                resolve(o);
-            });
-        }
-    });
-}
 
-function cyrb53(str, seed = 0) {
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for (let i = 0, ch; i < str.length; i++) {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-    return 4294967296 * (2097151 & h2) + (h1>>>0);
-};
