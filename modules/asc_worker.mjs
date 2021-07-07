c29b9c746e65dcc01b44c65e15298d6dba9fb5bb
Craig Gorman <craig@gormantec.com>
2021-07-07 13:47:07 +1000

    xxxx

--- modules/asc_worker.mjs
+++ modules/asc_worker.mjs
@@ -1,6 +1,10 @@
 importScripts("https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js");
 
-
+function callback(e,d)
+{
+    console.log("e:"+e);
+    console.log("d:"+d);
+}
 
 onmessage = function (e) {
 
