957a685e141d3bda3c6e77c75c7cf79675a30a0e
Craig Gorman <craig@gormantec.com>
2021-07-07 13:30:51 +1000

    sss

--- modules/asc_worker.mjs
+++ modules/asc_worker.mjs
@@ -1,10 +1,13 @@
 importScripts("https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js");
 
-console.log(require);
+//console.log(require);
 
 onmessage = function (e) {
 
     console.log("worker 1: "+e.data);
+    require(["https://cdn.jsdelivr.net/npm/assemblyscript@latest/dist/sdk.js"], ({ asc }) => {
+
+    });
 
 }
 
