be95bbabd0d284f4e9beb016ab3a0933a217ae38
Craig Gorman <craig@gormantec.com>
2021-07-07 13:10:25 +1000

    xxx

--- modules/asc_worker.mjs
+++ modules/asc_worker.mjs
@@ -1,4 +1,3 @@
-import { getScript } from '/modules/getScript.mjs';
 const getRequire = getScript('https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js', ["require"]);
 
 onmessage = function (e) {
@@ -8,5 +7,48 @@ onmessage = function (e) {
         require(["https://cdn.jsdelivr.net/npm/assemblyscript@latest/dist/sdk.js"], ({ asc }) => {
             console.log("worker 2");
         });
+    });
+}
+
+
+/** FUNCTIONS */
+async function getScript(src, objects) {
+    return new Promise((resolve, reject) => {
+        var script = window.document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = src;
+        script.async = 'true';
+        script.id = cyrb53(src);
+        var exists = document.getElementById(script.id);
+        if (exists) {
+            if (resolved[script.id]) {
+                resolve(resolved[script.id]);
+            }
+            else {
+                document.getElementById(script.id).addEventListener('load', function () {
+                    var o = {};
+                    if (objects && objects.length > 0) {
+                        objects.forEach((o1) => {
+                            o[o1] = window[o1];
+                        })
+                    }
+                    resolved[script.id] = o;
+                    resolve(o);
+                });
+            }
+        }
+        else {
+            document.getElementsByTagName('head')[0].appendChild(script);
+            script.addEventListener('load', function () {
+                var o = {};
+                if (objects && objects.length > 0) {
+                    objects.forEach((o1) => {
+                        o[o1] = window[o1];
+                    })
+                }
+                resolved[script.id] = o;
+                resolve(o);
+            });
+        }
     });
 }
