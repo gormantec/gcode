aa5091012cfe5e5aa834162e857813b793938907
Craig Gorman <craig@gormantec.com>
2021-07-08 17:22:22 +1000

    xxx

--- modules/asc_worker.mjs
+++ modules/asc_worker.mjs
@@ -9,7 +9,6 @@ console.log(requireModule);
 console.log(requireModule.require);
 console.log(requireModule.exports);
 
-console.log(asc);
 console.log(ascModule);
 console.log(ascModule.require);
 console.log(ascModule.exports);
