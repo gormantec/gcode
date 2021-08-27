34a7bd86421c773b5dc55f9fbaf4bdee5aa04979
Craig Gorman <craig@gormantec.com>
2021-08-27 17:22:41 +1000

    xxx

--- modules/wasmdom/imports.js
+++ modules/wasmdom/imports.js
@@ -273,6 +273,7 @@ export function init(window, _fetch, _Response) {
               //console.log("fetch Pointer="+p+ " Response="+r);
               
               console.log("exec then=\"Contract\"");
+              console.log(res);
               var r = getPointer(res);
               _wasm.__alertPromiseJSContract(p, r,_wasm.__pin(_wasm.__newString(res.accountId.toString())),_wasm.__pin(_wasm.__newString(res.contractId.toString())));
             }
