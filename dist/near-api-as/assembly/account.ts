158589403bc2fe5c8cf5c1c43cac157aecfd3b75
Craig Gorman <craig@gormantec.com>
2021-08-27 23:57:31 +1000

    xxxxx

--- dist/near-api-as/assembly/account.ts
+++ dist/near-api-as/assembly/account.ts
@@ -3,18 +3,25 @@ import { Connection } from "./connection";
 import {  Promise, JSObject } from "wasmdom-globals";
 import { near_login,consoleLog } from "./near-api-as";
 
+let waitFlag:i32=0;
+
 export class Account {
     readonly connection: Connection;
     readonly accountId: string;
+    myPromise:Promise;
 
     constructor(connection: Connection, accountId: string) {
         this.connection = connection;
         this.accountId = accountId;
-        let p:Promise=new Promise(near_login(accountId));
-        p.thenJSObject((o:JSObject)=>{
+        this.myPromise=new Promise(near_login(accountId));
+        //this.myPromise.
+        waitFlag=1;
+        this.myPromise.thenJSObject((o:JSObject)=>{
+            waitFlag=0;
             consoleLog("thenJSObject");
             return null;
         });
+        while(waitFlag==1){let r=Math.random()*Math.random()*Math.random()*Math.random();}
     }
 
 }
