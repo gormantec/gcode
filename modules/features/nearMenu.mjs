90b540af96a373149eade585cb8fa5f8722c71fe
gormantec <craig@gormantec.com>
2021-11-25 12:15:32 +1100

    xxx

--- modules/features/nearMenu.mjs
+++ modules/features/nearMenu.mjs
@@ -58,7 +58,7 @@ export function dialogAction(event) {
             });
         }
         else if (event.value == "test") {
-            test({ accountId: accountId, contractId: contractId });
+            test({ accountId: accountId, contractId: contractId ,methods:[{method:"setGreeting",type:"changeMethods",parameters:{"message":"hello"}}]});
         }
     }
     else if (event.type == "select" && event.id == "nearDialogSelect") {
