e5c978b6ded557904d83465af5eae72b697bdf8c
gormantec <craig@gormantec.com>
2022-01-05 09:12:47 +1100

    ggg

---
+++ modules/features/publishPWA.mjs
@@ -0,0 +1,36 @@
+export function menuAction() {
+
+    document.getElementById("publishPwaDialog").showModal();
+}
+
+
+export const dialogMetadata = [
+    {
+        "id": "publishPwaDialog",
+        "content": [
+            {
+                "id": "publishPwaDialogSelect", "type": "select", "label": "Action:", "options": [
+                    { "value": "publish", "text": "Test", "selected": true }
+                ]
+            },
+        ],
+        "ok": { "value": "publish" }//default value
+    },
+    {
+        "id": "publishPwaDialogTimer",
+        "type": "timer"
+    }
+];
+
+export function dialogAction(event) {
+
+    if (event.type == "dialog" && event.id == "publishPwaDialog") {
+    }
+    else if (event.type == "select" && event.id == "publishPwaDialogSelect") {
+        event.setInputValue("confirmButton", event.value);
+    }
+}
+
+export function afterLoad() {
+
+}
