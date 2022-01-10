c4f82289edc6d7eb386cdc67ef29576beaa64ff6
gormantec <craig@gormantec.com>
2022-01-10 14:35:06 +1100

    vvvv

--- modules/features/publishPWA.mjs
+++ modules/features/publishPWA.mjs
@@ -112,87 +112,86 @@ function publishToGit(code, user,token)
                     let slib=load(importFiles[i].dir+importFiles[i].name);
                     if(slib && typeof slib=="string" && slib.length>0)
                     {
-                        filesArray.push({ name: "/lib/"+importFiles[i].name, data: slib, type: "string" });
+                        filesArray.push({ name: "/lib/"+importFiles[i].name, data: window.btoa(slib), type: "base64" });
                     }       
                 }
                 console.log(filesArray);
-            });
-
-
-
-
-            var result = createHtml(code);
-            var splashBackgroundColor = result.splashBackgroundColor;
-            var splash = result.splash;
-            var mockFrame = result.mockFrame;
-            var rootHTML = result.rootHTML; errorline=333;
-
-            var _module = window.document.createElement("script");
-            _module.setAttribute("type", "module");
-            _module.text = "\n" + window.editor.getValue() + "\n";
-            rootHTML.querySelector("head").appendChild(_module);
-            var wpos = "top=50,left=50";errorline=339;
-            var w = 375;
-            var h = 896 * 375 / 414; //iphoneX=896/414
-            var mockPadding = 40;
-            if (screen.height <= 768) {
-                w = Math.floor(w * 0.75);
-                h = Math.floor(h * 0.75);
-                mockPadding = Math.floor(mockPadding * 0.75);
-                wpos = "top=0,left=0";
-            }
-            var wh = "width=" + parseInt(w) + ",height=" + parseInt(h); errorline=349;
-            var frame = "";
-            if (mockFrame) {
-                wh = "width=" + (w + mockPadding) + ",height=" + (h + mockPadding);
-                frame = "?mockFrame=" + mockFrame;errorline=353;
-            }
-            if (!win || win.closed) {
-                win = window.open("", "_blank", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no," + wh + "," + wpos);
-                if (splashBackgroundColor) win.document.body.style.backgroundColor = splashBackgroundColor;
-                else win.document.body.style.backgroundColor = "black";errorline=358;
-            }
-            var uploadConfig={ gituser:user+"-"+appName,gittoken:token, html: "<!DOCTYPE html>\n" + rootHTML.outerHTML, icon: splash  };
-            if(result.icon192x192)uploadConfig.icon192=result.icon192x192;
-            if(result.icon512x512)uploadConfig.icon512=result.icon512x512;
-            console.log(uploadConfig);
-            _uploadFile(uploadConfig, function (error, uri) {
-                if (error) { errorline=361;
-                    window.debug.log(error); errorline=362;
+                var result = createHtml(code);
+                var splashBackgroundColor = result.splashBackgroundColor;
+                var splash = result.splash;
+                var mockFrame = result.mockFrame;
+                var rootHTML = result.rootHTML; errorline=333;
+    
+                var _module = window.document.createElement("script");
+                _module.setAttribute("type", "module");
+                _module.text = "\n" + window.editor.getValue() + "\n";
+                rootHTML.querySelector("head").appendChild(_module);
+                var wpos = "top=50,left=50";errorline=339;
+                var w = 375;
+                var h = 896 * 375 / 414; //iphoneX=896/414
+                var mockPadding = 40;
+                if (screen.height <= 768) {
+                    w = Math.floor(w * 0.75);
+                    h = Math.floor(h * 0.75);
+                    mockPadding = Math.floor(mockPadding * 0.75);
+                    wpos = "top=0,left=0";
                 }
-                else {
-                    window.debug.log("open window"); errorline=365;
-                    win.location.href = uri + frame; errorline=366;
-
-                    var accountIdList=code.match(/\"*accountId\"*\s*?:\s*?\"[a-zA-Z0-9_-]*?\.(testnet|mainnet)\"/gi);errorline=368;
-                    if(accountIdList && accountIdList.length>0)
-                    {
-                        var accountId=accountIdList[0].replace(/\"*accountId\"*\s*?:\s*?\"([a-zA-Z0-9_-]*?\.)(testnet|mainnet)\"/gi,"$1$2");
-                        import('/modules/near/nearConfig.mjs').then(({ nearConfig }) => {errorline=372;
-                            const getNearApi = getScript('https://cdn.jsdelivr.net/npm/near-api-js@0.41.0/dist/near-api-js.min.js', ["nearApi"]);errorline=373;
-                            getNearApi.then(({ nearApi }) => {errorline=374;
-                                const nearCfg = nearConfig(nearApi);errorline=375;console.log("get key:"+accountId);
-                                nearCfg.keyStore.getKey("testnet", accountId).then((key) => {errorline=376;console.log("key="+key);
-                                    if(key)
-                                    {
-                                        const lll = function (e) {errorline=377;
-                                            console.log("Received Post: " + e.origin);errorline=378;
-                                            if (e.origin !== "https://s3-ap-southeast-2.amazonaws.com") return;
-                                            console.log("Send Post to: " + uri);
-                                            win.postMessage({ accountId: accountId, key: key.toString() }, uri);
-                                            window.removeEventListener("message", lll);
-                                            console.log("Send Post");
-                                        };
-                                        window.addEventListener("message", lll, false);
-                                    }
-                                })
-                            })
-                        });
+                var wh = "width=" + parseInt(w) + ",height=" + parseInt(h); errorline=349;
+                var frame = "";
+                if (mockFrame) {
+                    wh = "width=" + (w + mockPadding) + ",height=" + (h + mockPadding);
+                    frame = "?mockFrame=" + mockFrame;errorline=353;
+                }
+                if (!win || win.closed) {
+                    win = window.open("", "_blank", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no," + wh + "," + wpos);
+                    if (splashBackgroundColor) win.document.body.style.backgroundColor = splashBackgroundColor;
+                    else win.document.body.style.backgroundColor = "black";errorline=358;
+                }
+                var uploadConfig={ gituser:user+"-"+appName,gittoken:token, html: "<!DOCTYPE html>\n" + rootHTML.outerHTML, icon: splash  };
+                if(result.icon192x192)uploadConfig.icon192=result.icon192x192;
+                if(result.icon512x512)uploadConfig.icon512=result.icon512x512;
+                if(filesArray && filesArray.length>0)uploadConfig.filesArray=filesArray;
+                console.log(uploadConfig);
+                _uploadFile(uploadConfig, function (error, uri) {
+                    if (error) { errorline=361;
+                        window.debug.log(error); errorline=362;
                     }
-
-                }
-
+                    else {
+                        window.debug.log("open window"); errorline=365;
+                        win.location.href = uri + frame; errorline=366;
+    
+                        var accountIdList=code.match(/\"*accountId\"*\s*?:\s*?\"[a-zA-Z0-9_-]*?\.(testnet|mainnet)\"/gi);errorline=368;
+                        if(accountIdList && accountIdList.length>0)
+                        {
+                            var accountId=accountIdList[0].replace(/\"*accountId\"*\s*?:\s*?\"([a-zA-Z0-9_-]*?\.)(testnet|mainnet)\"/gi,"$1$2");
+                            import('/modules/near/nearConfig.mjs').then(({ nearConfig }) => {errorline=372;
+                                const getNearApi = getScript('https://cdn.jsdelivr.net/npm/near-api-js@0.41.0/dist/near-api-js.min.js', ["nearApi"]);errorline=373;
+                                getNearApi.then(({ nearApi }) => {errorline=374;
+                                    const nearCfg = nearConfig(nearApi);errorline=375;console.log("get key:"+accountId);
+                                    nearCfg.keyStore.getKey("testnet", accountId).then((key) => {errorline=376;console.log("key="+key);
+                                        if(key)
+                                        {
+                                            const lll = function (e) {errorline=377;
+                                                console.log("Received Post: " + e.origin);errorline=378;
+                                                if (e.origin !== "https://s3-ap-southeast-2.amazonaws.com") return;
+                                                console.log("Send Post to: " + uri);
+                                                win.postMessage({ accountId: accountId, key: key.toString() }, uri);
+                                                window.removeEventListener("message", lll);
+                                                console.log("Send Post");
+                                            };
+                                            window.addEventListener("message", lll, false);
+                                        }
+                                    })
+                                })
+                            });
+                        }
+    
+                    }
+    
+                });
             });
+
+
 
 
         }
