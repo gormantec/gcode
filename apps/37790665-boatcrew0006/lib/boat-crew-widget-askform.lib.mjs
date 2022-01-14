2894df9d695ff6d2601d85100c375013488630f4
gormantec <37790665+gormantec@users.noreply.github.com>
2022-01-14 11:01:28 +1100

    commit

--- apps/37790665-boatcrew0006/lib/boat-crew-widget-askform.lib.mjs
+++ apps/37790665-boatcrew0006/lib/boat-crew-widget-askform.lib.mjs
@@ -70,7 +70,7 @@ export class AskDivForm extends DivForm {
                 }
             ],
             sendButton: {
-                onclick: sendAskButtonAction
+                onclick: this.sendAskButtonAction
             }
         });
     }
