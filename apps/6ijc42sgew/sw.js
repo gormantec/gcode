3e42ea9e95d0939ffff2ad8d12445bda56fa42ec
gormantec <37790665+gormantec@users.noreply.github.com>
2022-01-14 11:11:33 +1100

    commit

---
+++ apps/6ijc42sgew/sw.js
@@ -0,0 +1,13 @@
+const PRECACHE = 'precache-v1-6ijc42sgew';
+const RUNTIME = 'runtime-6ijc42sgew';
+const PRECACHE_URLS = [
+  '/apps/6ijc42sgew/index.html',
+  '/apps/6ijc42sgew/index.html?mockFrame=iphoneX',
+  '/apps/6ijc42sgew/icon.png',
+  '/apps/6ijc42sgew/192x192.icon.png',
+  '/apps/6ijc42sgew/512x512.icon.png',
+  '/apps/6ijc42sgew/manifest.json',
+  'https://gcode.com.au/css/pwa.css'
+];
+self.addEventListener('install', event => {event.waitUntil(caches.open(PRECACHE).then(cache => cache.addAll(PRECACHE_URLS)).then(self.skipWaiting()));});
+self.addEventListener('activate', event => {const currentCaches = [PRECACHE, RUNTIME]; event.waitUntil(caches.keys().then(cacheNames => cacheNames.filter(cacheName => !currentCaches.includes(cacheName))).then(cachesToDelete => Promise.all(cachesToDelete.map(cacheToDelete =>caches.delete(cacheToDelete)))).then(() => self.clients.claim()));});self.addEventListener('fetch', event => { if (event.request.url.startsWith(self.location.origin) || event.request.url.startsWith('https://gcode.com.au')) { event.respondWith( caches.match(event.request).then(cachedResponse => { if (false && cachedResponse) { console.log(cachedResponse);return cachedResponse;} else return caches.open(RUNTIME).then(cache =>fetch(event.request).then(response => cache.put(event.request, response.clone()).then(() =>response)));}));}});
