cdc59c940694d594136e4a27aa4e719b14f0f889
gormantec <craig@gormantec.com>
2022-01-30 23:52:42 +1100

    xxxx

--- modules/pwa.mjs
+++ modules/pwa.mjs
@@ -744,7 +744,7 @@ class AuthButtons extends Div {
             }
         });
         this.appendChild(authButtons);
-        if (params && (params.skip=="true" || params.skip==true)) this.appendChild(new Div({child:new ActionButton({marginLeft:"-98px",right:"unset",width:"185px",borderRadius:"2px",innerText:"SKIP",onclick:()=>{myglobals.PWA.setPage(_nextPage);}})}));
+        if (params && (params.skip=="true" || params.skip==true)) this.appendChild(new Div({child:new ActionButton({marginLeft:"-98px",right:"unset",width:"185px",borderRadius:"2px",backgroundColor:"#999999",innerText:"SKIP",onclick:()=>{myglobals.PWA.setPage(_nextPage);}})}));
     }
 }
 class ActionButton extends Div{
