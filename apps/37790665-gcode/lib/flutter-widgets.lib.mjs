25dd591f3ef38251afd59e18df1248747f561238
gormantec <37790665+gormantec@users.noreply.github.com>
2022-02-27 08:54:14 +1100

    commit

--- apps/37790665-gcode/lib/flutter-widgets.lib.mjs
+++ apps/37790665-gcode/lib/flutter-widgets.lib.mjs
@@ -4,13 +4,16 @@ import { Div } from 'https://gcode.com.au/modules/pwa.mjs';
 export class Row extends Div
 {
       constructor(params) {
-        super({"position":"relative","bottom":"unset","height":"auto","children":params.children});
+        super({"position":"relative","bottom":"unset","height":"30px","children":params.children});
       }
 }
 export class Column extends Div
 {
       constructor(params) {
-        super({"position":"relative","display":"inline","bottom":"unset","height":"auto","child":params.child});
+        super({"position":"relative","display":"inline","bottom":"unset","left":"unset","right":"unset","height":"30px","width":"30px"});
+        if(params instanceof Div) this.setChild(params);
+        else if(params.child) this.setChild(params.child);
+        
       }
 }
 export class OutlinedButton extends Div
@@ -18,7 +21,7 @@ export class OutlinedButton extends Div
       constructor(params) {
         super(params);
 		this.style.height = "30px";
-        this.style.height = "30px";
+        this.style.width = "30px";
       }
 }
 export class Text extends Div
