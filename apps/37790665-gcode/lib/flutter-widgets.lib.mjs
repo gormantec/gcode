68ca108adb8387bd975efec24c19db115ba0c708
gormantec <37790665+gormantec@users.noreply.github.com>
2022-02-27 09:31:00 +1100

    commit

--- apps/37790665-gcode/lib/flutter-widgets.lib.mjs
+++ apps/37790665-gcode/lib/flutter-widgets.lib.mjs
@@ -28,6 +28,8 @@ export class OutlinedButton extends Div
         super(params);
 		this.style.height = "50px";
         this.style.width = "50px";
+        this.style.paddingTop ="50%";
+        this.style.backgroundColor="grey";
       }
 }
 export class Text extends Div
