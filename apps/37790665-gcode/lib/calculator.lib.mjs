bdd88b4f9eb33f574b554fdf50d65ef0f458b97a
gormantec <37790665+gormantec@users.noreply.github.com>
2022-02-27 11:51:09 +1100

    commit

---
+++ apps/37790665-gcode/lib/calculator.lib.mjs
@@ -0,0 +1,48 @@
+import {Div} from 'https://gcode.com.au/modules/pwa.mjs';
+
+class CalcClass {
+    constructor() {
+        this.listeners = [];
+    }
+
+
+    AC() {
+        this.updateListener("ACxxx");
+    }
+
+    Invert() {
+
+        console.log("Invert");
+    }
+
+    Perc() {
+
+        console.log("Perc");
+    }
+
+    Divide() {
+
+        console.log("Divide");
+    }
+    addChangeListener(aDiv) {
+        this.listeners.push(aDiv);
+    }
+    updateListener(value) {
+        for (var i = 0; i < this.listeners.length; i++) {
+          	console.log(i);
+            this.listeners[i].innerText = value;
+        }
+    }
+}
+
+export class CalcDiv extends Div {
+    constructor(params) {
+        super(params);
+        if (params.calc) {
+            params.calc.addChangeListener(this);
+        }
+    }
+}
+
+
+export var calc = new CalcClass();
