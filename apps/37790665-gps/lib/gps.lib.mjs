99a9cf3e8bb786cd3c08f6946034dcc222917dc2
gormantec <37790665+gormantec@users.noreply.github.com>
2022-02-28 15:55:13 +1100

    commit

--- apps/37790665-gps/lib/gps.lib.mjs
+++ apps/37790665-gps/lib/gps.lib.mjs
@@ -9,10 +9,14 @@ export class GpsDiv extends Div {
     updateLocation() {
         let _this=this;
             if (navigator.geolocation) {
+      			console.log("updateLocation +");
                 navigator.geolocation.getCurrentPosition((position) => {
+                  
+      			console.log("updateLocation ++");
                     _this.innerHTML = "Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude;
                 });
             } else {
+      			console.log("updateLocation -");
                 _this.innerHTML = "Geolocation not supported";
             }
         
