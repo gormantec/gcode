8610e38f16431855ee322458f54f21c7d25ef70b
gormantec <37790665+gormantec@users.noreply.github.com>
2022-02-28 17:09:27 +1100

    commit

--- apps/37790665-gps/lib/gps.lib.mjs
+++ apps/37790665-gps/lib/gps.lib.mjs
@@ -13,11 +13,14 @@ class GpsClass {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition((position) => {
                     _this.coordDiv.innerHTML = Math.floor(position.coords.latitude * 100000) / 100000 + "  " + Math.floor(position.coords.longitude * 100000) / 100000;
+                  	if(_this.imageDiv)_this.imageDiv.style.backgroundImage='url("https://maps.googleapis.com/maps/api/staticmap?center='+position.coords.latitude+','+position.coords.longitude+'&zoom=14&size=400x400&key=AIzaSyAhXf8mmpJpudbdhmHOW6YtmGY2YaLAAYU")';
+        
                 });
             } else {
                 _this.coordDiv.innerHTML = "Geolocation not supported";
             }
         }
+      //https://maps.googleapis.com/maps/api/staticmap?center=51.477222,0&zoom=14&size=400x400&key=AIzaSyA3kg7YWugGl1lTXmAmaBGPNhDW9pEh5bo&signature=ciftxSv4681tGSAnes7ktLrVI3g=
     }
 
     newCoordDiv(p) {
@@ -26,7 +29,7 @@ class GpsClass {
     }
     newImageDiv(p) {
         if (!this.imageDiv) this.imageDiv = new Div(p);
-        return this.imageDiv;
+      	return this.imageDiv;
     }
 }
 
