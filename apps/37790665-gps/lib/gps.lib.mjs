3218b926b8c31d507ccfc3aaf691e6e24c0f11ed
gormantec <37790665+gormantec@users.noreply.github.com>
2022-02-28 16:03:28 +1100

    commit

--- apps/37790665-gps/lib/gps.lib.mjs
+++ apps/37790665-gps/lib/gps.lib.mjs
@@ -1,5 +1,7 @@
 import { Div } from 'https://gcode.com.au/modules/pwa.mjs';
 
+let count=0;
+
 export class GpsDiv extends Div {
     constructor(p) {
         super(p);
@@ -9,9 +11,11 @@ export class GpsDiv extends Div {
 
     updateLocation() {
         let _this=this;
+      
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition((position) => {
-                    _this.innerHTML = "" + Math.floor(position.coords.latitude*10000)/10000 + " : " + Math.floor(position.coords.longitude*10000)/10000;
+                  count=count+1;
+                    _this.innerHTML = count+":" + Math.floor(position.coords.latitude*10000)/10000 + " : " + Math.floor(position.coords.longitude*10000)/10000;
                 });
             } else {
                 _this.innerHTML = "Geolocation not supported";
