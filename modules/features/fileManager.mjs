6df308c85af894b47c462242dc8a8f805282f3bc
gormantec <craig@gormantec.com>
2022-03-04 10:55:09 +1100

    xxxx

--- modules/features/fileManager.mjs
+++ modules/features/fileManager.mjs
@@ -83,7 +83,7 @@ export function dialogAction(event) {
                 reader.onload = (e)=>{
                     console.log(e.target.result);
                     let current=event.getInputValue("uploadFileDialogData");
-                    if(!current || current.tim()=="")current="{files:[]}";
+                    if(!current || current.tim()=="")current="{\"files\":[]}";
                     let currentJson=JSON.parse(current);
                     current.files.push({name:file.name,lastModified:file.lastModified,size:file.lastModified,type:file.type,base64:e.target.result});
                     event.setInputValue("uploadFileDialogData", JSON.stringify(current));
