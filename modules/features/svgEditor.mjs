4e114febd514f914c49a286c6559c0767e343c29
gormantec <craig@gormantec.com>
2022-03-05 20:22:54 +1100

    dddd

---
+++ modules/features/svgEditor.mjs
@@ -0,0 +1,48 @@
+/* Feature Name: Help Menu */
+
+export const menuMetadata =  {"id":"svgEditor","class":"pageLeftToolbarButton","materialIcon":"image"};
+
+
+let svgEditorVisible = false;
+
+function createPageDiv() {
+    let pageDiv = document.createElement("div");
+    pageDiv.innerHTML = "";
+    pageDiv.style.width = "100%";
+    pageDiv.style.height = "100%";
+    pageDiv.style.borderColor = "black";
+    pageDiv.style.padding = "10px";
+    pageDiv.style.margin = "0px";
+    pageDiv.className = "pageDiv CodeMirror cm-s-material-darker2";
+    pageDiv.id = "pageMiddle-" + menuMetadata.id;
+    return pageDiv;
+}
+
+function showSvgEditor() {
+    var source = window.editor.getValue();
+    document.getElementById("pageMiddle").querySelector(".CodeMirror").style.display = "none";
+    let rootMiddlePage = document.getElementById("pageMiddle-" + menuMetadata.id);
+    if (rootMiddlePage) rootMiddlePage.remove();
+    rootMiddlePage = createPageDiv();
+    let pageImg = document.createElement("img");
+    pageImg.src="data:image/svg+xml;utf8,"+source.replace(/\n/g, " ").replace(/\r/g, " ");;
+    rootMiddlePage.append(pageImg);
+}
+
+function hideSvgEditor() {
+    var pageDiv = document.getElementById("pageMiddle-" + menuMetadata.id);
+    if (pageDiv) pageDiv.remove();
+    document.getElementById("pageMiddle").querySelector(".CodeMirror").style.display = "";
+    if (window.editor) window.editor.refresh();
+}
+
+export function menuAction() {
+    if (!uiEditorVisible) {
+        showSvgEditor();
+        svgEditorVisible = true;
+    }
+    else {
+        hideSvgEditor();
+        svgEditorVisible = false;
+    }
+}
