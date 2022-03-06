bc85d1ef25cf083262122faa805f80609ea39816
gormantec <craig@gormantec.com>
2022-03-06 13:36:13 +1100

    cccc

--- modules/features/svgEditor.mjs
+++ modules/features/svgEditor.mjs
@@ -163,13 +163,29 @@ function showSvgEditor() {
     pageImg = document.createElement("img");
     pageImg.src="data:image/svg+xml;utf8,"+source.replace(/\n/g, " ").replace(/\r/g, " ");;
     
+    let c=source.match(/\<path[\s\S]*?fill=".*?"[\s\S]*?\>/g)[0];
+    if(!c)c="#AAAAAA";
+    else c=c.replace(/(\<path[\s\S]*?fill=").*?("[\s\S]*?\>)/g,"$2");
+    if(c.startsWith("%23"))c="#"+c.substring(3);
+
+    let bc=source.match(/\<rect[\s\S]*?fill=".*?"[\s\S]*?\>/g)[0];
+    if(!bc)bc="#AAAAAA";
+    else bc=bc.replace(/(\<rect[\s\S]*?fill=").*?("[\s\S]*?\>)/g,"$2");
+    if(bc.startsWith("%23"))bc="#"+bc.substring(3);
+
     let h=source.match(/\<[\s\S]*?height=".*?"[\s\S]*?\>/g)[0];
     if(!h)h="192";
     else h=h.replace(/(\<[\s\S]*?height=")(.*?)("[\s\S]*?\>)/g,"$2");
+
     let w=source.match(/\<[\s\S]*?width=".*?"[\s\S]*?\>/g)[0];
     if(!w)w="192";
     else h=h.replace(/(\<[\s\S]*?width=")(.*?)("[\s\S]*?\>)/g,"$2");
-    let { svgPanel, svgBody } = createSvgMenu({"color":"#FF0000","backgroundColor":"#0000FF","height":""+h,"width":""+w});
+
+    let br=source.match(/\<rect[\s\S]*?rx=".*?"[\s\S]*?\>/g)[0];
+    if(!br)br="3";
+    else br=br.replace(/(\<rect[\s\S]*?rx=").*?("[\s\S]*?\>)/g,"$2");
+
+    let { svgPanel, svgBody } = createSvgMenu({"color":c,"backgroundColor":bc,"height":""+h,"width":""+w,"borderRadius":""+br});
     rootMiddlePage.append(svgPanel);
     rootMiddlePage.append(pageImg);
     document.getElementById("pageMiddle").append(rootMiddlePage);
