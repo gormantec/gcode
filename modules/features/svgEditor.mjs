b6ab06d887a1599cab0a270d702d7f0dec15a367
gormantec <craig@gormantec.com>
2022-03-06 12:03:39 +1100

    ssss

--- modules/features/svgEditor.mjs
+++ modules/features/svgEditor.mjs
@@ -77,6 +77,38 @@ function appendSvgParams(svgBody, svgParams) {
         }
     }
 }
+function colorInput(input) {
+    input.type = "color";
+    var input2 = document.createElement("input");
+    input2.style.border = "none";
+    input2.size = 10;
+    input2.value = input.value;
+    var pageDivC1 = document.createElement("div");
+    pageDivC1.style.display = "inline-block";
+    pageDivC1.style.backgroundColor = "white";
+    pageDivC1.style.borderRadius = "2px";
+    pageDivC1.style.width = "220px";
+    pageDivC1.style.margin = "1px";
+    var pageDivC11 = document.createElement("div");
+    pageDivC11.style.display = "inline-block";
+    pageDivC11.style.marginTop = "3px";
+    pageDivC11.style.marginLeft = "3px";
+    pageDivC11.style.backgroundColor = "white";
+    pageDivC11.append(input2);
+
+    var pageDivC2 = document.createElement("div");
+    pageDivC2.style.display = "inline-block";
+    pageDivC2.style.float = "right";
+    pageDivC2.style.margin = "-2px";
+    pageDivC2.append(input);
+    pageDivC1.append(pageDivC11);
+    pageDivC1.append(pageDivC2);
+    input.addEventListener('change', function (evt) {
+        input2.value = this.value;
+    });
+    input = pageDivC1;
+    return input;
+}
 
 function createPageDiv() {
     let pageDiv = document.createElement("div");
