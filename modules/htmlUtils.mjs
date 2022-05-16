b0a3016f661ad836ed7df65a1e01db08f36b2586
gormantec <craig@gormantec.com>
2022-05-16 11:56:38 +1000

    fff

--- modules/htmlUtils.mjs
+++ modules/htmlUtils.mjs
@@ -267,7 +267,10 @@ export function createHtml(code, options) {
     if (!appName || appName == code) appName = "gcode App";
     var description = code.replace(/[\s\S]*?description:([\s\S]*?)((\n.*?[a-zA-Z0-9]*?\s*?:)|(\*\/))[\s\S]*/gm, '$1');
     if (!description || description == code) description = "A gcode developed PWA app";
-    description=description.trim().replace(/[\n ]*/g," ").trim();
+    console.log("description="+description);
+    description=description.trim();
+    description=description.replace("\n"," ");
+    console.log("description="+description);
     appName = appName.trim();
     var manifest = code.replace(/\/\*.*?manifest:.*?(.*\.json)[\n].*?\*\/.*/s, '$1');
     if (!manifest || manifest == "" || manifest == code) manifest = "xxxxx_manifest.json";
