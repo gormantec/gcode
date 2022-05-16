6c0f87cedfe56dc055adb4cd9f27f2e6185520b9
gormantec <37790665+gormantec@users.noreply.github.com>
2022-05-16 18:23:03 +1000

    commit (1652689379)

---
+++ apps/3nixt2fc78/sw.js
@@ -0,0 +1,15 @@
+const PRECACHE = 'precache-v1-3nixt2fc78';
+const RUNTIME = 'runtime-3nixt2fc78';
+const PRECACHE_URLS = [
+  '/apps/3nixt2fc78/index.html',
+  '/apps/3nixt2fc78/index.html?mockFrame=iphoneX',
+  '/apps/3nixt2fc78/icon.png',
+  '/apps/3nixt2fc78/splash.png',
+  '/apps/3nixt2fc78/180x180.icon.png',
+  '/apps/3nixt2fc78/192x192.icon.png',
+  '/apps/3nixt2fc78/512x512.icon.png',
+  '/apps/3nixt2fc78/manifest.json',
+  'https://gcode.com.au/css/pwa.css'
+];
+self.addEventListener('install', event => {event.waitUntil(caches.open(PRECACHE).then(cache =>{let r=null;try{r=cache.addAll(PRECACHE_URLS);}catch(e){consol.log(e);};return r;}).then(self.skipWaiting()));});
+self.addEventListener('activate', event => {const currentCaches = [PRECACHE, RUNTIME]; event.waitUntil(caches.keys().then(cacheNames => cacheNames.filter(cacheName => !currentCaches.includes(cacheName))).then(cachesToDelete => Promise.all(cachesToDelete.map(cacheToDelete =>caches.delete(cacheToDelete)))).then(() => self.clients.claim()));});self.addEventListener('fetch', event => { if (event.request.url.startsWith(self.location.origin) || event.request.url.startsWith('https://gcode.com.au')) { event.respondWith( caches.match(event.request).then(cachedResponse => { if (false && cachedResponse) { console.log(cachedResponse);return cachedResponse;} else return caches.open(RUNTIME).then(cache =>fetch(event.request).then(response => cache.put(event.request, response.clone()).then(() =>response)));}));}});
