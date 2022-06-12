7ee74e1ecc9ffef8013b3603e6fa92dc22b65a16
gormantec <craig@gormantec.com>
2022-06-12 17:44:22 +1000

    fff

--- modules/features/uiMenu.mjs
+++ modules/features/uiMenu.mjs
@@ -135,6 +135,7 @@ async function refreshScreen() {
 function structureToCode() {
 
     let resp = "";
+    console.log(structure);
 
     structure.forEach((block) => {
         if (block.comment) {
@@ -147,8 +148,8 @@ function structureToCode() {
             let params = block.widget.params;
             let rx = /^(.*?)(\S*?)(\s*?=\s*?new[\s]*?)(\S*?)(\s*?\()([\s\S]*?)\)/g
             let paramString = block.widget.code.trim().replaceAll(rx,"$1$2$3$4$5"+JSON.stringify(params, null, 4)+")");
-            let regex27 = /(^.*?)\"widget\(([\s\S]+?)\)\"(.*?$)/gm;
-            paramString = paramString.replaceAll(regex27, "$1$2$3");
+            let regex27 = /\"widget\(([\s\S]+)\)\"([,]?$)/gm;
+            paramString = paramString.replaceAll(regex27, "$1$2");
             let regex22 = /(:\s*?)\"(function\s*?\(.*?\)\s*?\{.*\})\"/g;
             paramString = paramString.replaceAll(regex22, "$1$2");
             let regex23 = /(:\s*?)\"(this\.\S*?)\"/g;
@@ -165,8 +166,9 @@ function structureToCode() {
                 'class ' + block.class.name + ' extends ' + block.class.extends + ' {\n    constructor() {\n        super(' +
                 JSON.stringify(params, null, 4).replaceAll("\n    ", "\n            ").slice(0, -1) +
                 '        });\n    }$3');
-            let regex27 = /(^.*?)\"widget\(([\s\S]+?)\)\"(.*?$)/gm;
-            paramString = paramString.replaceAll(regex27, "$1$2$3");
+            console.log(paramString);
+            let regex27 = /\"widget\(([\s\S]+)\)\"([,]?$)/gm;
+            paramString = paramString.replaceAll(regex27, "$1$2");
             let regex22 = /(:\s*?)\"(function\s*?\(.*?\)\s*?\{.*\})\"/g;
             paramString = paramString.replaceAll(regex22, "$1$2");
             let regex23 = /(:\s*?)\"(this\.\S*?)\"/g;
