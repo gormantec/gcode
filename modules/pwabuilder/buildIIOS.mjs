eda37a87f4ded2cbdaa0c6860c46c5e40e7df8c5
gormantec <craig@gormantec.com>
2022-07-02 15:04:23 +1000

    dddd

--- modules/pwabuilder/buildIIOS.mjs
+++ modules/pwabuilder/buildIIOS.mjs
@@ -22,6 +22,17 @@ const response = await fetch('https://pwabuilder-ios.azurewebsites.net/packages/
     }),
     headers: { 'Content-Type': 'application/json' }
 });
+console.log(JSON.stringify({
+    "name": _manifestJSON.name,
+    "bundleId": "au.com.gcode",
+    "url": "https://gcode.com.au"+_manifestJSON.start_url,
+    "imageUrl": "/apps/" + process.env.APP_NAME + "/512x512.icon.png",
+    "splashColor": _manifestJSON.background_color,
+    "progressBarColor": _manifestJSON.theme_color,
+    "statusBarColor": _manifestJSON.background_color,
+    "permittedUrls": [], "manifestUrl": "https://gcode.com.au/apps/" + process.env.APP_NAME + "/manifest.json",
+    "manifest": _manifestJSON
+}));
 if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 await streamPipeline(response.body, createWriteStream('./pwa.zip'));
 
