0201ff36775fc597990f9f6c7f64278fd5c1f6ee
gormantec <craig@gormantec.com>
2022-07-05 10:03:55 +1000

    xxxx

--- modules/pwabuilder/buildIIOS.mjs
+++ modules/pwabuilder/buildIIOS.mjs
@@ -7,23 +7,7 @@ console.log("https://gcode.com.au/apps/" + process.env.APP_NAME + "/manifest.jso
 const _manifest = await fetch("https://gcode.com.au/apps/" + process.env.APP_NAME + "/manifest.json");
 const _manifestJSON=await _manifest.json();
 const streamPipeline = promisify(pipeline);
-const response = await fetch('https://pwabuilder-ios.azurewebsites.net/packages/create', {
-    method: 'post',
-    body: JSON.stringify({
-        "name": _manifestJSON.name,
-        "bundleId": "au.com.gcode."+process.env.APP_NAME,
-        "url": "https://gcode.com.au"+_manifestJSON.start_url,
-        "imageUrl": "/apps/" + process.env.APP_NAME + "/512x512.icon.png",
-        "splashColor": _manifestJSON.background_color,
-        "progressBarColor": _manifestJSON.theme_color,
-        "statusBarColor": _manifestJSON.background_color,
-        "permittedUrls": ["https://isjqwevc8e.execute-api.ap-southeast-2.amazonaws.com/default/udid"],
-        "manifestUrl": "https://gcode.com.au/apps/" + process.env.APP_NAME + "/manifest.json",
-        "manifest": _manifestJSON
-    }),
-    headers: { 'Content-Type': 'application/json' }
-});
-console.log(JSON.stringify({
+const pwabuilderData={
     "name": _manifestJSON.name,
     "bundleId": "au.com.gcode.*",
     "url": "https://gcode.com.au"+_manifestJSON.start_url,
@@ -31,9 +15,16 @@ console.log(JSON.stringify({
     "splashColor": _manifestJSON.background_color,
     "progressBarColor": _manifestJSON.theme_color,
     "statusBarColor": _manifestJSON.background_color,
-    "permittedUrls": [], "manifestUrl": "https://gcode.com.au/apps/" + process.env.APP_NAME + "/manifest.json",
+    "permittedUrls":_manifestJSON.permitted_urls,
+    "manifestUrl": "https://gcode.com.au/apps/" + process.env.APP_NAME + "/manifest.json",
     "manifest": _manifestJSON
-}));
+}
+const response = await fetch('https://pwabuilder-ios.azurewebsites.net/packages/create', {
+    method: 'post',
+    body: JSON.stringify(pwabuilderData),
+    headers: { 'Content-Type': 'application/json' }
+});
+console.log(JSON.stringify(pwabuilderData));
 if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 await streamPipeline(response.body, createWriteStream('./pwa.zip'));
 
