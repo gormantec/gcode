583d1f0de5f4d963f7f8796aeae5d6a948cc0def
gormantec <craig@gormantec.com>
2022-07-05 22:51:38 +1000

    xxxx

--- modules/pwabuilder/buildIIOS.mjs
+++ modules/pwabuilder/buildIIOS.mjs
@@ -20,6 +20,9 @@ const pwabuilderData={
 }
 if(Array.isArray(_manifestJSON.permitted_urls) && _manifestJSON.permitted_urls.length>0)
 {
+    console.log("****************B");
+    console.log(_manifestJSON.permitted_urls);
+    console.log("****************B");
     pwabuilderData.permittedUrls=_manifestJSON.permitted_urls;
 }
 const response = await fetch('https://pwabuilder-ios.azurewebsites.net/packages/create', {
@@ -27,7 +30,9 @@ const response = await fetch('https://pwabuilder-ios.azurewebsites.net/packages/
     body: JSON.stringify(pwabuilderData),
     headers: { 'Content-Type': 'application/json' }
 });
+console.log("****************A");
 console.log(JSON.stringify(pwabuilderData));
+console.log("****************A");
 if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 await streamPipeline(response.body, createWriteStream('./pwa.zip'));
 
