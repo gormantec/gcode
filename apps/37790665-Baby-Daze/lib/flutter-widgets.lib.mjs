30a136b5dd61b189984ea0e641d09cb874197603
gormantec <37790665+gormantec@users.noreply.github.com>
2022-07-15 19:45:40 +1000

    commit (1657878334)

--- apps/37790665-Baby-Daze/lib/flutter-widgets.lib.mjs
+++ apps/37790665-Baby-Daze/lib/flutter-widgets.lib.mjs
@@ -1,78 +1,109 @@
-import { Div } from 'https://gcode.com.au/modules/pwa.mjs';
+import {
+    Div
+} from 'https://gcode.com.au/modules/pwa.mjs';
 
 /*---------------------------------------------------------------------------*/
-export class Row extends Div
-{
-      constructor(params) {
-        super({"position":"relative","top":"unset","bottom":"unset","left":"unset","right":"unset","children":params.children});
-        for(var i=0;i<this.childrenCount;i++)
-        {
-          this.element.children[i].style.width = (Math.floor(1000/this.childrenCount)/10)+"%";
+export class Row extends Div {
+    constructor(params) {
+        super({
+            "position": "relative",
+            "top": "unset",
+            "bottom": "unset",
+            "left": "unset",
+            "right": "unset",
+            "children": params.children
+        });
+        for (var i = 0; i < this.childrenCount; i++) {
+            this.element.children[i].style.width = (Math.floor(1000 / this.childrenCount) / 10) + "%";
         }
-      }
+    }
 }
-export class Column extends Div
-{
-      constructor(params) {
-        super({"position":"relative","display":"inline-block","top":"unset","bottom":"unset","left":"unset","right":"unset"});
-        if(params instanceof Div) this.setChild(params);
-        else if(params.child) this.setChild(params.child);
-		this.style.height = this.firstChild.style.height;
-      }
+export class Column extends Div {
+    constructor(params) {
+        super({
+            "position": "relative",
+            "display": "inline-block",
+            "top": "unset",
+            "bottom": "unset",
+            "left": "unset",
+            "right": "unset"
+        });
+        if (params instanceof Div) this.setChild(params);
+        else if (params.child) this.setChild(params.child);
+        this.style.height = this.firstChild.style.height;
+    }
 }
 
-export class OutlinedButton extends Div
-{
-      constructor(params) {
+export class OutlinedButton extends Div {
+    constructor(params) {
         super(params);
-		if(params.height) this.style.height = params.height;
+        if (params.height) this.style.height = params.height;
         else this.style.height = "80px";
         this.style.width = params.width || "80px";
-        this.style.cursor ="pointer";
-    	this.style.pointerEvents="auto";
-      }
-}
-
-export class Text extends Div
-{
-      constructor(_text) {
-        super({"position":"absolute",top:"0px",bottom:"0px",left:"0px",right:"0px",display: "flex",
-        alignItems: "center",
-        justifyContent: "center","child":new Div({"classNameOverride":true,"innerText":_text})});
-      }
-}
-export class ListView extends Div
-{
-  constructor(params)
-  {
-    super(params);
-    this.divChildren=params.children;
-    while (this.element.firstChild) {
-      this.element.removeChild(this.element.lastChild);
+        this.style.cursor = "pointer";
+        this.style.pointerEvents = "auto";
     }
-    for(let i=0;i<this.divChildren.length;i++)
-    {
-      console.log("x");
-      this.appendChild(new Div({"position":"relative",height:this.divChildren[i].style.height,child:this.divChildren[i]}));
+}
+
+export class Text extends Div {
+    constructor(_text) {
+        super({
+            "position": "absolute",
+            top: "0px",
+            bottom: "0px",
+            left: "0px",
+            right: "0px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            "child": new Div({
+                "classNameOverride": true,
+                "innerText": _text
+            })
+        });
     }
-  }
 }
-export class Container extends Div
-{
-  constructor(params)
-  {
-    super(params);
-  }
+export class ListView extends Div {
+    constructor(params) {
+        super(params);
+        this.divChildren = params.children;
+        while (this.element.firstChild) {
+            this.element.removeChild(this.element.lastChild);
+        }
+        for (let i = 0; i < this.divChildren.length; i++) {
+            console.log("x");
+            this.appendChild(new Div({
+                "position": "relative",
+                height: this.divChildren[i].style.height,
+                child: this.divChildren[i]
+            }));
+        }
+    }
 }
-export class Center extends Div
-{
-  constructor(params)
-  {
-    super(params);
-    this.element.style.display="flex";
-    this.element.style.alignItems="center";
-    this.element.style.justifyContent="center";
-  }
+export class Container extends Div {
+    constructor(params) {
+        super(params);
+    }
+}
+export class Center extends Div {
+    constructor(params) {
+        super(params);
+        this.element.style.display = "flex";
+        this.element.style.alignItems = "center";
+        this.element.style.justifyContent = "center";
+    }
 }
 
-
+export class ListTile extends Div {
+    constructor(params) {
+        super(params);
+    }
+}
+export class Icon extends Div {
+    constructor(params) {
+        super(params);
+    }}
+export const Icons = {
+  battery_full:"",
+  star:""
+}
