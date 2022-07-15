be5688ac68560585ff680202b47478455f0fedd6
gormantec <37790665+gormantec@users.noreply.github.com>
2022-07-15 16:40:15 +1000

    commit (1657867210)

---
+++ apps/37790665-Baby-Daze/sw.js
@@ -0,0 +1,23 @@
+const PRECACHE = 'precache-v1-37790665-Baby-Daze';
+const RUNTIME = 'runtime-37790665-Baby-Daze';
+const PRECACHE_URLS = [
+  '/apps/37790665-Baby-Daze/index.html',
+  '/apps/37790665-Baby-Daze/index.html?mockFrame=iphoneX',
+  '/apps/37790665-Baby-Daze/icon.png',
+  '/apps/37790665-Baby-Daze/splash.icon.png',
+  '/apps/37790665-Baby-Daze/180x180.icon.png',
+  '/apps/37790665-Baby-Daze/192x192.icon.png',
+  '/apps/37790665-Baby-Daze/512x512.icon.png',
+  '/apps/37790665-Baby-Daze/manifest.json',
+  'https://gcode.com.au/css/pwa.css'
+];
+self.addEventListener('install', event => {event.waitUntil(caches.open(PRECACHE).then(cache =>{
+  let r=null;
+  try{
+    r=cache.addAll(PRECACHE_URLS);
+  }catch(e){
+    console.log(e);
+  }
+  return r;
+}).then(self.skipWaiting()));});
+self.addEventListener('activate', event => {const currentCaches = [PRECACHE, RUNTIME]; event.waitUntil(caches.keys().then(cacheNames => cacheNames.filter(cacheName => !currentCaches.includes(cacheName))).then(cachesToDelete => Promise.all(cachesToDelete.map(cacheToDelete =>caches.delete(cacheToDelete)))).then(() => self.clients.claim()));});self.addEventListener('fetch', event => { if (event.request.url.startsWith(self.location.origin) || event.request.url.startsWith('https://gcode.com.au')) { event.respondWith( caches.match(event.request).then(cachedResponse => { if (false && cachedResponse) { console.log(cachedResponse);return cachedResponse;} else return caches.open(RUNTIME).then(cache =>fetch(event.request).then(response => cache.put(event.request, response.clone()).then(() =>response)));}));}});
