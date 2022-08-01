a752beef09f25bee9c3efd2ea92f02123d78dad5
gormantec <37790665+gormantec@users.noreply.github.com>
2022-08-01 10:57:07 +1000

    commit (1659315421)

--- apps/37790665-findmyapple/lib/flutter-widgets.lib.mjs
+++ apps/37790665-findmyapple/lib/flutter-widgets.lib.mjs
@@ -4,6 +4,8 @@ import {
     Page
 } from 'https://gcode.com.au/modules/pwa.mjs';
 
+import {BluetoothInterface} from 'https://gcode.com.au/modules/bluetoothInterface.mjs';
+
 /*---------------------------------------------------------------------------*/
 export class Row extends Div {
     constructor(params) {
@@ -802,6 +804,8 @@ export class BluetoothPage extends Page {
                 PWA.getPWA().hideHeader();
             }
         });
+      
+      	
 
         let appPlatform = this.getCookie("app-platform");
         if (appPlatform && !navigator.bluetooth) {
@@ -813,88 +817,7 @@ export class BluetoothPage extends Page {
                 this.appendPeripheral(e.detail);
             });
             window.webkit.messageResponse = window.webkit.messageResponse || {};
-
-            navigator.bluetooth = {
-                requestDevice: (d) => {
-                    let start = Date.now(),
-                        timeout = 60000,
-                        id = Math.floor(Math.random() * 16777215).toString(16) + '-' + Math.floor(Math.random() * 16777215).toString(16) + '-' + Date.now().toString(16),
-                        messagetype = 'bluetooth-request-device';
-                    window.webkit.messageHandlers[messagetype].postMessage({
-                        id: id,
-                        data: d
-                    });
-                    let responseType = "bluetooth-request-device-" + id;
-                    return new Promise((resolve, reject) => {
-                      	console.log("listen for:"+responseType);
-                        window.addEventListener(responseType, (e) => {
-                          console.log("event :"+responseType);
-                            resolve({
-                                id: e.detail.selectedPeripheralId,
-                                name: e.detail.selectedPeripheralName,
-                                addEventListener: (type, f) => {
-                                    if (type == 'gattserverdisconnected') {
-                                        window.webkit.messageHandlers[messagetype].postMessage({
-                                            id: id,
-                                            data: d
-                                        });
-                                    }
-                                },
-                                gatt: {
-                                    connect: () => new Promise((res1, rej1) => {
-                                        res1({
-                                            id: 'server',
-                                            getPrimaryServices: () => new Promise((res2, rej2) => {
-                                                res2([{
-                                                        uuid: 'service1',
-                                                        getCharacteristics: () => new Promise((res3, rej3) => {
-                                                            res3([{
-                                                                value: 'value1',
-                                                                properties: {
-                                                                    notify: true
-                                                                },
-                                                                addEventListener: (type, f) => {
-                                                                    console.log('addEventListener');
-                                                                },
-                                                                startNotifications: () => {
-                                                                    console.log('startNotifications');
-                                                                }
-                                                            }]);
-                                                        })
-
-                                                    },
-                                                    {
-                                                        uuid: 'service2',
-                                                        getCharacteristics: () => new Promise((res4, rej4) => {
-                                                            res4([{
-                                                                value: 'value1',
-                                                                properties: {
-                                                                    notify: true
-                                                                },
-                                                                addEventListener: (type, f) => {
-                                                                    console.log('addEventListener');
-                                                                },
-                                                                startNotifications: () => {
-                                                                    console.log('startNotifications');
-                                                                }
-                                                            }]);
-                                                        })
-
-                                                    }
-                                                ])
-                                            })
-                                        })
-
-                                    })
-                                }
-                            });
-                        });
-
-                    });
-                }
-            }
-
-
+            navigator.bluetooth = new BluetoothInterface();
         }
     }
 
