9fd829e279cc22ed116ab75affd254c8e7f75623
gormantec <craig@gormantec.com>
2022-08-01 14:25:27 +1000

    xxx

--- modules/bluetoothInterface.mjs
+++ modules/bluetoothInterface.mjs
@@ -1,8 +1,8 @@
 export class BluetoothDevice {
-    constructor(id,name) {
+    constructor({id,name}) {
         this.id= id;
         this.name= name;
-        this.gatt= new GattServerConnector(id, name)
+        this.gatt= new GattServerConnector({id:id, name:name})
     }
 
     addEventListener(type, f) {
@@ -15,7 +15,7 @@ export class BluetoothDevice {
 }
 
 export class PrimaryService {
-    constructor(uuid) {
+    constructor({uuid}) {
         this.uuid = uuid;
     }
     getCharacteristics() {
@@ -41,12 +41,12 @@ export class GattServer {
         this.id = 'server';
     }
     getPrimaryServices() {
-        new Promise((res2, rej2) => { res2([new PrimaryService("service1"), new PrimaryService("service2")]) });
+        new Promise((res2, rej2) => { res2([new PrimaryService({uuid:"service1"}), new PrimaryService({uuid:"service2"})]) });
     }
 }
 
 export class GattServerConnector {
-    constructor(id, name) {
+    constructor({id, name}) {
         this.id = id;
         this.name = name;
     }
@@ -62,21 +62,21 @@ export class BluetoothInterface {
 
     }
 
-    requestDevice(d) {
+    requestDevice({acceptAllDevices}) {
         let start = Date.now(),
             timeout = 60000,
             id = Math.floor(Math.random() * 16777215).toString(16) +
                 '-' + Math.floor(Math.random() * 16777215).toString(16) +
                 '-' + Date.now().toString(16),
             messagetype = 'bluetooth-request-device';
-        window.webkit.messageHandlers[messagetype].postMessage({id: id,data: d });
+        window.webkit.messageHandlers[messagetype].postMessage({id: id,data: {acceptAllDevices:acceptAllDevices} });
         let responseType = "bluetooth-request-device-" + id;
         return new Promise((resolve, reject) => {
             console.log("listen for:" + responseType);
             let eventListener = (e) => {
                 console.log("found: " + e);
                 window.removeEventListener(responseType, eventListener);
-                resolve(new BluetoothDevice(e.detail.selectedPeripheralId,e.detail.selectedPeripheralName));
+                resolve(new BluetoothDevice({id:e.detail.selectedPeripheralId,name:e.detail.selectedPeripheralName}));
             };
             window.addEventListener(responseType, eventListener);
         });
