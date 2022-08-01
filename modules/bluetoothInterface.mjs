21072e14ef641c50f4d0618d2e1748f3b2bd825c
gormantec <craig@gormantec.com>
2022-08-01 11:43:31 +1000

    ffff

--- modules/bluetoothInterface.mjs
+++ modules/bluetoothInterface.mjs
@@ -20,69 +20,70 @@ export class BluetoothInterface {
         let responseType = "bluetooth-request-device-" + id;
         return new Promise((resolve, reject) => {
             console.log("listen for:" + responseType);
-            window.addEventListener(responseType, (e) => {
-                console.log("event :" + responseType);
-                resolve({
-                    id: e.detail.selectedPeripheralId,
-                    name: e.detail.selectedPeripheralName,
-                    addEventListener: (type, f) => {
-                        if (type == 'gattserverdisconnected') {
-                            window.webkit.messageHandlers[messagetype].postMessage({
-                                id: id,
-                                data: d
-                            });
-                        }
-                    },
-                    gatt: {
-                        connect: () => new Promise((res1, rej1) => {
-                            res1({
-                                id: 'server',
-                                getPrimaryServices: () => new Promise((res2, rej2) => {
-                                    res2([{
-                                        uuid: 'service1',
-                                        getCharacteristics: () => new Promise((res3, rej3) => {
-                                            res3([{
-                                                value: 'value1',
-                                                properties: {
-                                                    notify: true
-                                                },
-                                                addEventListener: (type, f) => {
-                                                    console.log('addEventListener');
-                                                },
-                                                startNotifications: () => {
-                                                    console.log('startNotifications');
-                                                }
-                                            }]);
-                                        })
+            window.addEventListener(responseType,this.deviceRequestResult);
 
-                                    },
-                                    {
-                                        uuid: 'service2',
-                                        getCharacteristics: () => new Promise((res4, rej4) => {
-                                            res4([{
-                                                value: 'value1',
-                                                properties: {
-                                                    notify: true
-                                                },
-                                                addEventListener: (type, f) => {
-                                                    console.log('addEventListener');
-                                                },
-                                                startNotifications: () => {
-                                                    console.log('startNotifications');
-                                                }
-                                            }]);
-                                        })
-
-                                    }
-                                    ])
+        });
+    }
+    deviceRequestResult(e){
+        console.log(e);
+        resolve({
+            id: e.detail.selectedPeripheralId,
+            name: e.detail.selectedPeripheralName,
+            addEventListener: (type, f) => {
+                if (type == 'gattserverdisconnected') {
+                    window.webkit.messageHandlers[messagetype].postMessage({
+                        id: id,
+                        data: d
+                    });
+                }
+            },
+            gatt: {
+                connect: () => new Promise((res1, rej1) => {
+                    res1({
+                        id: 'server',
+                        getPrimaryServices: () => new Promise((res2, rej2) => {
+                            res2([{
+                                uuid: 'service1',
+                                getCharacteristics: () => new Promise((res3, rej3) => {
+                                    res3([{
+                                        value: 'value1',
+                                        properties: {
+                                            notify: true
+                                        },
+                                        addEventListener: (type, f) => {
+                                            console.log('addEventListener');
+                                        },
+                                        startNotifications: () => {
+                                            console.log('startNotifications');
+                                        }
+                                    }]);
                                 })
-                            })
 
+                            },
+                            {
+                                uuid: 'service2',
+                                getCharacteristics: () => new Promise((res4, rej4) => {
+                                    res4([{
+                                        value: 'value1',
+                                        properties: {
+                                            notify: true
+                                        },
+                                        addEventListener: (type, f) => {
+                                            console.log('addEventListener');
+                                        },
+                                        startNotifications: () => {
+                                            console.log('startNotifications');
+                                        }
+                                    }]);
+                                })
+
+                            }
+                            ])
                         })
-                    }
-                });
-            });
+                    })
 
+                })
+            }
         });
     }
 
