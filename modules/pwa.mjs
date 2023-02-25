20cfbf7cdd7ebf704036fa006e99cee433e0c39a
gormantec <craig@gormantec.com>
2023-02-26 08:25:39 +1100

    dddfff

--- modules/pwa.mjs
+++ modules/pwa.mjs
@@ -22,7 +22,8 @@ class PWA {
         this.footerHeight = params.footerHeight || 40;
         this.footerPadding = params.footerPadding || (this.footerHeight - 10) / 2;
         this.headerFontSize = params.headerFontSize || 24;
-        this.primaryColorText = this.getTextColor(this.primaryColor);
+
+        this.primaryColorText = params.primaryColorText || this.getTextColor(this.primaryColor);
         this.footer = params.footer || "<a href=\"https://gcode.com.au\">gcode()</a> by gormantec";
         this.innerHTML = "";
         this.pwaRoot = new Div({ id: "pwaroot" });
